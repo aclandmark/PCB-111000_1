93da72758f092cbf695002158f647685a4fb7d81
aclandmark <mark.osborne15@btinternet.com>
2019-12-01 21:24:26 +0000

    Create Stream_chars_subroutines.c

---
+++ System_programs/Basic_proj/AVR_projects/Stream_chars_AVR/Stream_chars_subroutines.c
@@ -0,0 +1,99 @@
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <stdint.h>
+#include <stdio.h>
+
+void I2C_Tx(char, char, char*);
+void send_byte_with_Ack(char);
+void send_byte_with_Nack(char);
+
+#define waiting_for_I2C_master \
+TWCR = (1 << TWEA) | (1 << TWEN);\
+while (!(TWCR & (1 << TWINT)));\
+TWDR;
+
+
+
+/************************************************************************************/
+void I2C_Tx_2_integers(unsigned int s1, unsigned int s2){			
+char num_bytes=4; char mode=4; char s[4];
+for (int m = 0;  m < 4; m++){
+switch (m){
+case 0: s[m] = s1; break; 											//Send S1 lower byte
+case 1: s[m] = s1 >> 8; break; 									//Send S1 higher byte									
+case 2: s[m] = s2; 	break;											//Send S1 lower byte
+case 3: s[m] = s2 >> 8; break;}}									//Send S1 higher byte
+I2C_Tx(num_bytes,mode, s);}
+
+
+
+/************************************************************************************/
+void I2C_Tx(char num_bytes, char mode, char s[]){
+waiting_for_I2C_master;
+send_byte_with_Ack(num_bytes);
+send_byte_with_Ack(mode);
+for (int m = 0;  m < num_bytes; m++){
+if (m==num_bytes-1){send_byte_with_Nack(s[m]);}
+else {send_byte_with_Ack(s[m]);}}
+TWCR = (1 << TWINT);}
+
+
+
+/************************************************************************************/
+void Timer_T1_sub(char Counter_speed, unsigned int Start_point){ 
+TCNT1H = (Start_point >> 8);
+TCNT1L = Start_point;
+TCCR1B = Counter_speed;
+while(!(TIFR1 & (1<<TOV1)));
+TIFR1 |= (1<<TOV1); 
+TCCR1B = 0;}
+
+/************************************************************************************/
+void send_byte_with_Ack(char byte){
+TWDR = byte;													//Send payload size: Zero in this case
+TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);				//clear interrupt and set Enable Acknowledge
+while (!(TWCR & (1 << TWINT)));}
+
+
+
+/************************************************************************************/
+void send_byte_with_Nack(char byte){
+TWDR = byte;													//Send payload size: Zero in this case
+TWCR = (1 << TWINT) | (1 << TWEN);								//clear interrupt and set Enable Acknowledge
+while (!(TWCR & (1 << TWINT)));}
+
+
+
+/************************************************************************************/
+unsigned int PRN_16bit_GEN(unsigned int start){
+unsigned int bit, lfsr;
+
+if(!(start)) lfsr = (eeprom_read_byte((uint8_t*)(0x3EF)) << 8) + eeprom_read_byte((uint8_t*)(0x3EE));
+else lfsr = start;
+bit = (( lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1;
+lfsr = (lfsr >> 1) | (bit << 15);
+if(!(start)){
+eeprom_write_byte((uint8_t*)(0x3EF),(lfsr>>8));
+eeprom_write_byte((uint8_t*)(0x3EE),lfsr);}
+
+return lfsr;}
+	
+
+
+/************************************************************************/
+void USART_init (unsigned char UBRROH_N, unsigned char UBRR0L_N ){
+UCSR0B = 0;
+UBRR0H = UBRROH_N;  									
+UBRR0L = UBRR0L_N;  								
+UCSR0A = (1 << U2X0);
+UCSR0B = (1 << RXEN0) | (1<< TXEN0);
+UCSR0C =  (1 << UCSZ00)| (1 << UCSZ01);}
+
+
+/*********************************************************************/
+void Char_to_PC(char data){
+while (!(UCSR0A & (1 << UDRE0)));
+UDR0 = data;}
