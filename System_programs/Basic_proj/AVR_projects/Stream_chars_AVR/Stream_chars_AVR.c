334dfbbcb28cdbe627755088d2611d7edb3cfae3
aclandmark <mark.osborne15@btinternet.com>
2020-01-03 17:44:54 +0000

    Update Stream_chars_AVR.c

--- System_programs/Basic_proj/AVR_projects/Stream_chars_AVR/Stream_chars_AVR.c
+++ System_programs/Basic_proj/AVR_projects/Stream_chars_AVR/Stream_chars_AVR.c
@@ -11,28 +11,20 @@
 
 
 void I2C_Tx_2_integers(unsigned int, unsigned int);
-
 void Timer_T1_sub(char, unsigned int);
-unsigned int PRN_16bit_GEN(unsigned int );
-
-
 void USART_init (unsigned char, unsigned char);
 void Char_to_PC(char);
 
 
 
-
-
-
 /*****************************************************************************/
-
 #include "Stream_chars_header_file.h"
 #include "Stream_chars_subroutines.c"
 
+
+
 int main (void){								
 unsigned int n=33;
-
-
 
 CLKPR = (1 << CLKPCE);
 CLKPR = (1 << CLKPS0);
@@ -44,5 +36,4 @@ Unused_I_O;
 USART_init(0,16);
 
 while(1){
-while(n < 127){Char_to_PC(n++);Timer_T1_sub(T1_delay_100ms);}n = 33;}
-}
+while(n < 127){Char_to_PC(n++);Timer_T1_sub(T1_delay_100ms);}n = 33;}}
