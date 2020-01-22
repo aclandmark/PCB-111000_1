f33a7bcac09d3d57c6e95233f2da116be4cdf563
aclandmark <mark.osborne15@btinternet.com>
2020-01-22 12:42:00 +0000

    Create Proj_1A1.ino

---
+++ UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A1_LED_display_UNO/Proj_1A1/Proj_1A1.ino
@@ -0,0 +1,25 @@
+  
+/*Proj_1A1_LED_display
+******************************************************/
+
+
+
+
+
+
+#include "Proj_1A1_header_file.h"
+
+
+int main (void)
+{                       //start of main routine
+unsigned int PORT_1;              
+setup_HW;
+
+while(1){                   //While loop starts here
+  PORT_1=1;                 //In the absence of a "SW_reset", "PORT_1" mast be re-initialised every time the loop is repeated
+  for(int m = 1; m < 17; m++){        //for loop starts here
+    I2C_Tx_2_integers(PORT_1, PORT_1);
+    Timer_T0_10mS_delay_x_m(6);
+    PORT_1 = PORT_1 << 1;}          //for loop ends here
+    }                   //while loop ends here
+}                       //end of main loop
