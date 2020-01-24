758f0ef598fb87ec781b055243d099e8be496b8a
aclandmark <mark.osborne15@btinternet.com>
2020-01-24 21:57:43 +0000

    Update Proj_1C1_UNO.ino

--- UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1C1_Pin_Change_Interupt_UNO/Proj_1C1_UNO/Proj_1C1_UNO.ino
+++ UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1C1_Pin_Change_Interupt_UNO/Proj_1C1_UNO/Proj_1C1_UNO.ino
@@ -1,48 +1,40 @@
 
-/*Proj_1C1_Pin_Change_Interupt
-**************************************************************
+/*Proj_1C_LED_display    
 
-A SLIGHT VARIATION ON PROGRAM 3 THE REACTION TIME TESTER.
-INTRODUCES the statement "if(m == 1)" which means execute the next statement if m equals 1, and
-if it equals anything else skip the next statement.
-*/
-
-
+Testing your reaction time
+************************************************************/
 
 #include "Proj_1C1_header_file.h"
 
+volatile unsigned int PORT_1, mask;         //Variables used by both the main routine and also by the ISR 
+char switch_control;                        //Prevents rapid switch_3 presses from shooting leds down at random  
 
-
-volatile unsigned int PORT_1, mask;      
-char switch_control;                        //Prevents rapid switch_3 presses from shooting leds down at random
 
 int main (void){
 setup_HW;
-config_sw3_for_PCI;                  
+config_sw1_and_sw2_for_PCI;                   //SW1 is not used         
 
-mask = 0xFFFF;                              //0xFFFF = 0b1111111111111111 indicating that none of the leds have yet been shot down
+mask = 0xFFFF;                                //0xFFFF = 0b1111111111111111 indicating that none of the leds have yet been shot down
 switch_control = 0;
-sei();                            
-while(mask){            
-PORT_1=1;             
+sei();                                       //Enable all interrupts             
+while(mask){                                  //Exit the "while-loop" as soon as mask gets set to zero
+PORT_1=1;                                     //Initialise display to 0000 0000 0000 0001
 
-for(int m = 1; m < 17; m++){    
-if(m == 1)switch_control = 0;                //Set switch_control to zero at the start of each sweep
+for(int m = 1; m < 17; m++){                  //Repeat "for-loop" 16 times
+if(m == 1)switch_control = 0;                 //Set switch_control to zero at the start of each sweep
 I2C_Tx_2_integers
-(PORT_1 & mask, (~mask) ^ PORT_1);
+(PORT_1 & mask, (~mask) ^ PORT_1);            //LOGIC: "Dead" leds are transferred to the bottom row
  
-Timer_T0_10mS_delay_x_m(10);    
-PORT_1 = (PORT_1 << 1);           
-}}I2C_Tx_2_integers(0, 0xFFFF); 
-Timer_T0_10mS_delay_x_m(100);          
+Timer_T0_10mS_delay_x_m(10);                  //Program execution spends most time waiting here, so this is where the interrupt almost always occurs            
+PORT_1 = (PORT_1 << 1);                       //Move on to next display location      
+}}I2C_Tx_2_integers(0, 0xFFFF);               //When all leds are dead illuminate all the bottom leds and then
+Timer_T0_10mS_delay_x_m(100);                 //pause for 1 sec before starting all over again.      
 SW_reset;}
 
 
-
-/**************Routine executed every time that switch is  operated**************************************/
-                  
-ISR(PCINT0_vect) {          
-if(switch_3_up)return;         
-if (switch_control) return;               //Return early if "switch_control" is 1
-mask &= ~PORT_1;          
-switch_control = 1;}                      //Set switch conrol to 1.
+/**************ISR Routine executed every time that switch 3 is  operated**************************************/
+ISR(PCINT2_vect) {                            //This ISR momentarily interrupts the main routine
+if(switch_2_up)return;                        //It notes which LED has just been shot down and 
+if (switch_control) return;                   //Return early if "switch_control" is 1
+mask &= ~PORT_1;                              //writes zero at its location in the "mask" register
+switch_control = 1;}                          //Set switch conrol to 1.
