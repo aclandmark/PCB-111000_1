d5ac1b8f7194d824a4a3f6d4c3d254cbd5901f1a
aclandmark <mark.osborne15@btinternet.com>
2020-01-25 18:01:46 +0000

    Update Proj_1A_UNO.ino

--- UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A_LED_display_UNO/Proj_1A_UNO/Proj_1A_UNO.ino
+++ UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A_LED_display_UNO/Proj_1A_UNO/Proj_1A_UNO.ino
@@ -1,12 +1,10 @@
 
 /*Proj_1A_LED_display
 ***************************************************/
-
-
- 
-
 #include "Proj_1A_header_file.h"          //Provides details of resources provided by the complier and project_SW
                                           //including "setup_HW", the I2C subroutine and "SW_reset"
+
+/********************Version 1**************************************************************/
 
 
 int main (void){                          //The "main" routine lies between a pair of {} brackets.
@@ -23,3 +21,18 @@ setup_HW;
     PORT_1 = PORT_1 << 1;                 //The contents of address "PORT_1" are shifted one place left
     }SW_reset;                            //every time that the "for-loop" is executed
 }
+
+
+/***********************ALTERNATIVE FORM************************************************/
+/*                                        //Start of comment section which is ignored by compiler
+int main (void)
+{unsigned int PORT_1;                    //start of main routine           
+setup_HW;
+while(1){                   				      //While loop starts here
+  PORT_1=1;                 				      //In the absence of a "SW_reset", "PORT_1" mast be re-initialised every time the loop is repeated
+  for(int m = 1; m < 17; m++){        		//for loop starts here
+    I2C_Tx_2_integers(PORT_1, PORT_1);
+    Timer_T0_10mS_delay_x_m(6);
+    PORT_1 = PORT_1 << 1;}}}          		//All loops including main end here
+*/                                        //End of comment section
+ 
