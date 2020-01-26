5d4cee2245330f8c32562531889095c99ee4a04a
aclandmark <mark.osborne15@btinternet.com>
2020-01-26 20:50:13 +0000

    Update Proj_2C1_UNO.ino

--- UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2C1_random_LEDs_UNO/Proj_2C1_UNO/Proj_2C1_UNO.ino
+++ UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2C1_random_LEDs_UNO/Proj_2C1_UNO/Proj_2C1_UNO.ino
@@ -3,25 +3,24 @@
 /*Proj_2C1_random_LEDs
 ************************************************************/
 
-/*Variation on Proj_2C_circulating_LEDs
-Uses a random number generator to fully populate the display.
-Uses basic WDT without interrupt.*/
-
 
 
 #include "Proj_2C1_header_file.h"
 
 char display_bkp[7];                                            //One element to backup each segment letter 
+char Dimmer_control;
 
 int main (void){
 char segment=0, digit_num=0, seg_counter = 0,direction = 0; 
 
 setup_HW;
-wdt_enable(WDTO_4S);                                           //WDT prevents display from being completed in either direction
+wdt_enable(WDTO_2S);                                           //WDT prevents display from being completed in either direction
 
+I2C_Tx_any_segment_clear_all();                                //Initialise display
 
 UCSR0B |= (1 << RXCIE0);                                       //Set Interrupt on key press (for test purposes only)
 sei();                                                         //Global enable interrupt
+Dimmer_control = 1;
 
 
 while(1){                                                      //Generate pattern
@@ -38,7 +37,7 @@ if ((direction) && (!(display_bkp[segment - 'a'] & (1 << digit_num)))) continue;
 
 I2C_Tx_any_segment(segment, digit_num);                        //Update display
 backup_the_display(segment, digit_num);                        //keep backup up to date
-Timer_T0_10mS_delay_x_m(10);  wdr();                            //delay and reset watch dog
+Timer_T0_10mS_delay_x_m(5);  wdr();                            //delay and reset watch dog
 seg_counter += 1;}
 
 direction ^= 1;                                                //Toggle the direction_counter value
@@ -55,6 +54,18 @@ display_bkp[segment - 'a'] = display_bkp[segment - 'a'] ^ (1 << digit_num);}
 
 
 /*****************************************************************/
-ISR(USART_RX_vect){receiveChar();
-I2C_Tx_any_segment_clear_all();
-sei();while(1);}  
+ISR(USART_RX_vect){
+receiveChar();
+I2C_Tx(1, 'Q', &Dimmer_control);}  
+
+
+/*Local version of subroutine "I2C_Tx()"
+
+void I2C_Tx_local(char num_bytes, char mode, char* s){
+waiting_for_I2C_master;                                       //Turn on I2C slave and await call from master
+send_byte_with_Ack(num_bytes);                                //send data byte, request acknowledgement
+send_byte_with_Ack(mode);
+for (int m = 0;  m < num_bytes; m++){
+if (m==num_bytes-1){send_byte_with_Nack(s[m]);}               //Last byte, no ackowledgement needed
+else {send_byte_with_Ack(s[m]);}}
+TWCR = (1 << TWINT);}                                         //Clear interrupt and close I2C slave*/
