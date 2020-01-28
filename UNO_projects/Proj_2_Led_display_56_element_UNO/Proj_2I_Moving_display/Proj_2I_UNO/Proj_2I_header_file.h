da88d22fe81192af635b0b6426e62bfd0ceba777
aclandmark <mark.osborne15@btinternet.com>
2020-01-28 21:23:38 +0000

    Create Proj_2I_header_file.h

---
+++ UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2I_Moving_display/Proj_2I_UNO/Proj_2I_header_file.h
@@ -0,0 +1,25 @@
+
+
+#include "Resources_V12/Header_files/Basic_HW_setup.h"
+#include "Resources_V12/Subroutines/Basic_HW_plus_Timer.c"
+#include "Resources_V12/I2C_Subroutines/I2C_subroutines_1.c"
+#include "Resources_V12/Subroutines/Basic_PC_comms.c"
+#include "Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+
+#define zero "abcdef"					//chars a,b,c,d,e,f stored in array named "zero"
+#define one "bc"
+#define two "abged"
+#define three "abcdg"
+#define four "fgbc"
+#define five "afgcd"
+#define six "fegdc"
+#define seven "abc"
+#define eight "abcdefg"
+#define nine "abcfg"
+#define null '\0'
+
+void display_num_string (const char*, int);
+char digit_array_to_display(const char*, char);
+
+#define rotate_display_left  for(int m = 16; m; m--)digit_array[m] = digit_array[m-1];\
+digit_array[0] = digit_array[16];
