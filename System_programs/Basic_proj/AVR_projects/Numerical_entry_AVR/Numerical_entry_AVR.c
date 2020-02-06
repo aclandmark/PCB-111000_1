27dff38ff828416f6546fc91a6867ce4f3803ec8
aclandmark <mark.osborne15@btinternet.com>
2020-02-06 15:51:16 +0000

    Update Numerical_entry_AVR.c

--- System_programs/Basic_proj/AVR_projects/Numerical_entry_AVR/Numerical_entry_AVR.c
+++ System_programs/Basic_proj/AVR_projects/Numerical_entry_AVR/Numerical_entry_AVR.c
@@ -93,4 +93,12 @@ default: break;}char_ptr++;}}								//incrementing "char_ptr" steps through the
 															//Selecting segment letters in turn
 
 
+void Timer_T0_10mS_delay_x_m(int m)
+{for (int n = 0; n < m; n++){Timer_T0_sub(T0_delay_10ms);}}
+
+void Timer_T0_sub(char Counter_speed, unsigned char Start_point){ 
+TCNT0 = Start_point;
+TCCR0B = Counter_speed;
+while(!(TIFR0 & (1<<TOV0)));
+TIFR0 |= (1<<TOV0); TCCR0B = 0;}
 
