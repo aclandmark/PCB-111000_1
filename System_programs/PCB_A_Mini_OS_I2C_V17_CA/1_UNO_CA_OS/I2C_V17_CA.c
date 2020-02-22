cc761c93e1b8ad907cb6343619a5b95c6cfdd6d2
aclandmark <mark.osborne15@btinternet.com>
2020-02-22 21:07:17 +0000

    Update I2C_V17_CA.c

--- System_programs/PCB_A_Mini_OS_I2C_V17_CA/1_UNO_CA_OS/I2C_V17_CA.c
+++ System_programs/PCB_A_Mini_OS_I2C_V17_CA/1_UNO_CA_OS/I2C_V17_CA.c
@@ -27,12 +27,12 @@ the user projects.  The PIC only offered the slave implementation of the I2C bus
 0x3F6	cmd_counter_H
 0x3F5	cmd_counter_L
 0x3F4	Reset_status_2: Zero resets UNO immediately after PCB_A has been programmed triggering the h/t/r/D prompt
-
+0x3F3	8 bit PRN gen EEP subroutines
 
 
 ***********************/
 
-# include "I2C_V16_1_Header_file_CA.h"
+# include "I2C_V17_Header_file_CA.h"
 
 
 int main (void){
