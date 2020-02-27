05419cab73a4fd9bd7646a83b6af9739d9b0c2c0
aclandmark <mark.osborne15@btinternet.com>
2020-02-27 13:27:05 +0000

    Update Proj_3E_header_file.h

--- UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_3E_Data_from_KBD_UNO/Proj_3E_UNO/Proj_3E_header_file.h
+++ UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_3E_Data_from_KBD_UNO/Proj_3E_UNO/Proj_3E_header_file.h
@@ -5,7 +5,6 @@
 #include "Resources_V12/I2C_Subroutines/I2C_subroutines_1.c"
 #include "Resources_V12/I2C_Subroutines/I2C_subroutines_2.c"
 #include "Resources_V12/Subroutines/Basic_PC_comms.c"
-#include "Resources_V12/Subroutines/Arithmetic.c"
 #include "Resources_V12/Subroutines/Integers_to_from_PC.c"
 #include "Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c"
 
