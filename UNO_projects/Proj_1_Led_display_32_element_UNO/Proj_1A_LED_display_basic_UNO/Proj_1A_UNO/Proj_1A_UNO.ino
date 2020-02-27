ec755f17f7431b8861643e5c238230546ce3c800
aclandmark <mark.osborne15@btinternet.com>
2020-02-27 13:28:49 +0000

    Update Proj_1A_UNO.ino

--- UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A_LED_display_basic_UNO/Proj_1A_UNO/Proj_1A_UNO.ino
+++ UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A_LED_display_basic_UNO/Proj_1A_UNO/Proj_1A_UNO.ino
@@ -1,17 +1,20 @@
 
-/*Proj_1A_LED_display
-***************************************************/
+/**************Proj_1A_LED_display***************************
+Basic LED display********************************************/
+
+
 #include "Proj_1A_header_file.h"          //Provides details of resources provided by the complier and project_SW
-                                          //including "setup_HW", the I2C subroutine and "SW_reset"
+                                          //including "setup_HW", which sets up the device hard ware,
+                                          //the I2C subroutine and "SW_reset" (soft ware reset).
+
+
 
 /********************Version 1**************************************************************/
-
-
 int main (void){                          //The "main" routine lies between a pair of {} brackets.
 
 unsigned int PORT_1=1;                    //PORT_1 is an address in data memory initially set to 1 (binary 0000000000000001)
                                           //Note: that PORT_1 holds 16 bits (binary digits) of data 
-setup_HW;         
+setup_HW;     
 
   for(int m = 1; m < 17; m++){            //Code between the {} brackets is repeated 16 times             
                                           //as m increments from 1 to 16  (m++ is shorthand for increment m)
@@ -34,5 +37,5 @@ while(1){                   				      //While loop starts here
     I2C_Tx_2_integers(PORT_1, PORT_1);
     Timer_T0_10mS_delay_x_m(6);
     PORT_1 = PORT_1 << 1;}}}          		//All loops including main end here
-*/                                        //End of comment section
- 
+                                          //End of comment section
+*/
