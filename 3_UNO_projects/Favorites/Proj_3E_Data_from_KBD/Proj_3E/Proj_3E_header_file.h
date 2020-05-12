b2c1ced9e778ad7674ae456f723e820e4a71866a
aclandmark <mark.osborne15@btinternet.com>
2020-05-12 20:35:15 +0100

    Update Proj_3E_header_file.h

--- 3_UNO_projects/Favorites/Proj_3E_Data_from_KBD/Proj_3E/Proj_3E_header_file.h
+++ 3_UNO_projects/Favorites/Proj_3E_Data_from_KBD/Proj_3E/Proj_3E_header_file.h
@@ -1,6 +1,7 @@
 
 
 #include "Resources_V15/Header_files/Basic_HW_setup.h"
+#include "Resources_V15/Header_files/I_O_from_switches_UNO.h"
 #include "Resources_V15/Subroutines/Basic_HW_plus_Timer.c"
 #include "Resources_V15/I2C_Subroutines/I2C_subroutines_1.c"
 #include "Resources_V15/I2C_Subroutines/I2C_subroutines_2.c"
