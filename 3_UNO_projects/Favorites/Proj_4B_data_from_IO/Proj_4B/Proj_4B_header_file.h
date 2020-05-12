d90615e1a25f01a07e4de0cd0e2bbe98eb9e0004
aclandmark <mark.osborne15@btinternet.com>
2020-05-12 20:35:28 +0100

    Update Proj_4B_header_file.h

--- 3_UNO_projects/Favorites/Proj_4B_data_from_IO/Proj_4B/Proj_4B_header_file.h
+++ 3_UNO_projects/Favorites/Proj_4B_data_from_IO/Proj_4B/Proj_4B_header_file.h
@@ -20,9 +20,9 @@
 #define message_1 \
 "\r\n\
 Press sw1 to populate digits[0]\r\n\
-sw3 to shift display left\r\n\
-sw2 to enter the number\r\n\
+sw2 to shift display left\r\n\
+sw3 to enter the number\r\n\
 sw1 to multiply the number by 10\r\n\
-sw3 to divide it by 10\r\n\
-press sw2 before sw1 or 3 to repeat\r\n\
+sw2 to divide it by 10\r\n\
+press sw3 before sw1 or 2 to repeat\r\n\
 enter 0 to reset.\r\n"
