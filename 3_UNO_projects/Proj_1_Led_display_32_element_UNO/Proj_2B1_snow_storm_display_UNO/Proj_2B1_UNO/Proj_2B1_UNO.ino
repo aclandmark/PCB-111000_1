c9630343bc7a6807da9f102c8367b0533b1f9195
aclandmark <mark.osborne15@btinternet.com>
2020-05-14 14:34:15 +0100

    Update Proj_2B1_UNO.ino

--- 3_UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_2B1_snow_storm_display_UNO/Proj_2B1_UNO/Proj_2B1_UNO.ino
+++ 3_UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_2B1_snow_storm_display_UNO/Proj_2B1_UNO/Proj_2B1_UNO.ino
@@ -13,7 +13,7 @@ unsigned int PRN;                                 //Global memory location used 
 char Dimmer_control;
 
 int main (void){  
-setup_HW;
+setup_UNO;
 wdt_enable(WDTO_250MS);                           //Following a WD reset the PRN is re-initialised to 0xFFFF
 
 config_sw1_and_sw2_for_PCI;                       //SW1 is not used
