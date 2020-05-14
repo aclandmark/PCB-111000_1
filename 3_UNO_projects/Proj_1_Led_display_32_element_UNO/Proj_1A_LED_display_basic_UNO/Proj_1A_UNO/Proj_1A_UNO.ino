88ed12aef5937afb400ebefd1ace552fbecb1341
aclandmark <mark.osborne15@btinternet.com>
2020-05-14 14:33:54 +0100

    Update Proj_1A_UNO.ino

--- 3_UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A_LED_display_basic_UNO/Proj_1A_UNO/Proj_1A_UNO.ino
+++ 3_UNO_projects/Proj_1_Led_display_32_element_UNO/Proj_1A_LED_display_basic_UNO/Proj_1A_UNO/Proj_1A_UNO.ino
@@ -10,11 +10,11 @@ Basic LED display********************************************/
 
 
 /********************Version 1**************************************************************/
-/*int main (void){                          //The "main" routine lies between a pair of {} brackets.
+int main (void){                          //The "main" routine lies between a pair of {} brackets.
 
 unsigned int PORT_1=1;                    //PORT_1 is an address in data memory initially set to 1 (binary 0000000000000001)
                                           //Note: that PORT_1 holds 16 bits (binary digits) of data 
-setup_HW;     
+setup_UNO;     
 
   for(int m = 1; m < 17; m++){            //Code between the {} brackets is repeated 16 times             
                                           //as m increments from 1 to 16  (m++ is shorthand for increment m)
@@ -24,13 +24,13 @@ setup_HW;
     PORT_1 = PORT_1 << 1;                 //The contents of address "PORT_1" are shifted one place left
     }SW_reset;                            //every time that the "for-loop" is executed
 }
-*/
+
 
 /***********************ALTERNATIVE FORM************************************************/
-                                        //Start of comment section which is ignored by compiler
+/*                                        //Start of comment section which is ignored by compiler
 int main (void)
 {unsigned int PORT_1;                    //start of main routine           
-setup_HW;
+setup_UNO;
 while(1){                   				      //While loop starts here
   PORT_1=1;                 				      //In the absence of a "SW_reset", "PORT_1" mast be re-initialised every time the loop is repeated
   for(int m = 1; m < 17; m++){        		//for loop starts here
@@ -38,3 +38,5 @@ while(1){                   				      //While loop starts here
     Timer_T0_10mS_delay_x_m(6);
     PORT_1 = PORT_1 << 1;}}}          		//All loops including main end here
                                           //End of comment section
+
+ */
