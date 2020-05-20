60200990cbdedb36333dbd701142046790c3407a
aclandmark <mark.osborne15@btinternet.com>
2020-05-20 09:09:19 +0100

    Update Proj_2E.ino

--- 3_UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2E_Segment_entry_1/Proj_2E/Proj_2E.ino
+++ 3_UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2E_Segment_entry_1/Proj_2E/Proj_2E.ino
@@ -11,13 +11,12 @@ INTRODUCES
 1.  Use of "isCharavailable()" and "receiveChar()" in place of "waitforkeypress()" when waiting for 
 keyboard entry so that the watch dog timer can be implemented.
 
-2.  A second alternative to "waitforkeypress()" that can be used with the watch dog.  
-Consider the following program segment:
+A typical implementation is illustrated in the following program segment:  
 
-while(1){if(isCharavailable(10)){keypress = receiveChar();break;}}  
+    while(1){if(isCharavailable(10)){keypress = receiveChar();break;}}  
 
-Inclusion of a "wdr();" statement in "isCharavailable(10)" (as in Proj_2E) 
-would enable the watch dog timer to be used.
+Inclusion of a "wdr();" statement in "isCharavailable(10)" 
+enables the watch dog timer to be used.
 
 Note: 
 "waitforkeypress()" is really only suitable to simplify examples.  It pauses program execution 
@@ -25,26 +24,14 @@ indefinitely and prevents use of the watch dog timer which would time out and ge
 The WDT is not implemented in the mini-OS for these projects as it is considered that this would 
 introduce unnecessary complexity. 
 
-3.  "SW_reset" combined with detection of a watch dog time out to change the introductory text message.
-Note "SW_reset" works by enabling the WD timer and letting it time out.
-
-4.  The "#define" statement used to define text for use with the “String_to_PC()” subroutine.
-
-5.  “&&” a logical operation.
-For example if(A && B){do this} which means
-if (expression A is not zero and expression B is not zero) {do this}
-But if one or both of these expressions equates to zero continue without doing it. 
-
-6.  The "do{program segment}while(condition);" loop
-which keep repeating the program segment as long as the condition is non-zero.*/
-
+3.  SW_reset_with_interrupt:  This is a "SW_reset" combined with a WDT ISR.  The ISR writes to an 
+EPROM location which the main program reads shortly after every reset. It can there distinguish
+betweeen WDTouts and other resets. 
+*/
 
 
 
 #include "Proj_2E_header_file.h"
-
-
-
 
 
 int main (void){
@@ -52,28 +39,28 @@ int main (void){
 char letter = 0, digit_num;           
 
 setup_UNO;
-User_prompt;
 config_sw1_and_sw2_for_PCI;
 
-                            //Check watch_dog reset:
-if (!(watch_dog_reset))                  //If its zero we have a Normal start up 
-String_to_PC("Press key a to g (x for next digit)");
-                            //
-else {watch_dog_reset = 0;                //If its 1 we have a WD time out
-String_to_PC("  ?");                  //Abbreviated user prompt
-I2C_Tx_any_segment('h', 0);}              //Re-establish display
+if (Arduino_non_WDTout)
+  {User_prompt;
+  String_to_PC("Press key a to g (x for next digit)\r\n\
+Press SW1 for WDTout with interrupt");}
+
+else 
+  {clear_Arduino_WDT_flag
+  String_to_PC("  ?");                                          //Abbreviated user prompt
+  I2C_Tx_any_segment('h', 0);}                                  //Re-establish display
 
 sei();
-wdt_enable(WDTO_30MS);                  //Note: No WDT ISR and no effort to isolate WDT reset 
+SW_reset_with_interrupt;
+
 
 while(1){digit_num=0;
-
 while(digit_num < 8){
-
-if(isCharavailable_Local(4))              //Wait up to 60mS for a key press.  In the absence of one
-{switch(letter = receiveChar()){            //the program reverts back to "while(digit_num < 8){"
-case 'a': case 'A':                   //otherwise it continues and updates the display
-case 'b': case 'B':                   //Program execution is not put on hold awaiting a keypress
+if(isCharavailable_Local(4))                                //Wait up to 60mS for a key press.  In the absence of one
+{switch(letter = receiveChar()){                            //the program reverts back to "while(digit_num < 8){"
+case 'a': case 'A':                                         //otherwise it continues and updates the display
+case 'b': case 'B':                                         //Program execution is not put on hold awaiting a keypress
 case 'c': case 'C':
 case 'd': case 'D':
 case 'e': case 'E':
@@ -90,17 +77,20 @@ default: break;}}}}}
 
 
 char isCharavailable_Local (char m){int n = 0;
-while (!(UCSR0A & (1 << RXC0))){n++;          //Increments "n" from zero to 8000
-if (n>8000) {m--;n = 0;wdr();}if (m == 0)return 0;} //Checks the receiver at each increment
-return 1;}                        //Returns a 1 as soon as a character is received
-                            //If no character is sent it pauses a while (10m mS)
-                            //and eventually returns zero.
-                            //Note: this subroutine enables us to reset the WDT (wdr();)
-                            //in a way that is not possible with "waitforkeypress()"
-
-//Note: "char isCharavailable()" does not contain the "wdr();" command
+while (!(UCSR0A & (1 << RXC0))){n++;                        //Increments "n" from zero to 8000
+if (n>8000) {m--;n = 0;wdr();}if (m == 0)return 0;}         //Checks the receiver at each increment
+return 1;}                                                  //Returns a 1 as soon as a character is received
+                                                            //If no character is sent it pauses a while (10m mS)
+                                                            //and eventually returns zero.
+                                                            //Note: this subroutine enables us to reset the WDT (wdr();)
+                                                            //in a way that is not possible with "waitforkeypress()"
 
 /***********************************************************************/
 
 ISR(PCINT2_vect) 
-{if (switch_1_down) while(1);}
+{if (switch_1_down) {sei(); while(1);}}
+
+
+
+/*****************************************************************/
+ISR (WDT_vect){set_Arduino_WDTout;}                                       
