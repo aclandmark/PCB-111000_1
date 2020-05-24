b1681e70ea5795082ceffc7d76fd328612d99a43
aclandmark <mark.osborne15@btinternet.com>
2020-05-24 20:50:28 +0100

    Update Proj_7C_header_file.h

--- 3_UNO_projects/Proj_7_Real_numbers/Proj_7C_fixed_point_arithmetic/Proj_7C/Proj_7C_header_file.h
+++ 3_UNO_projects/Proj_7_Real_numbers/Proj_7C_fixed_point_arithmetic/Proj_7C/Proj_7C_header_file.h
@@ -1,17 +1,17 @@
 
 
-#include "../../Resources_V12/Header_files/Basic_HW_setup.h"
-#include "../../Resources_V12/Subroutines/Basic_HW_plus_Timer.c"
-#include "../../Resources_V12/I2C_Subroutines/I2C_subroutines_1.c"
-#include "../../Resources_V12/Subroutines/Basic_PC_comms.c"
-#include "../../Resources_V12/I2C_Subroutines/I2C_subroutines_2.c"
+#include "Resources_V15/Header_files/Basic_HW_setup.h"
+#include "Resources_V15/Subroutines/Basic_HW_plus_Timer.c"
+#include "Resources_V15/I2C_Subroutines/I2C_subroutines_1.c"
+#include "Resources_V15/Subroutines/Basic_PC_comms.c"
+#include "Resources_V15/I2C_Subroutines/I2C_subroutines_2.c"
 
-#include "../../Resources_V12/Subroutines/Integers_to_from_PC.c"
-#include "../../Resources_V12/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+#include "Resources_V15/Subroutines/Integers_to_from_PC.c"
+#include "Resources_V15/I2C_Subroutines/I2C_slave_Rx_Tx.c"
 
-#include "../../Resources_V12/Header_files/I_O_from switches.h"
+#include "Resources_V15/Header_files/I_O_from_switches_UNO.h"
 
-#include "../../Resources_V12/Subroutines/Arithmetic.c"
-#include "../../Resources_V12/Subroutines/Floating_point_arithmetic.c"
+#include "Resources_V15/Subroutines/Arithmetic.c"
+#include "Resources_V15/Subroutines/Floating_point_arithmetic.c"
 
-#include "../../Resources_V12/Subroutines/Real_numbers_to_from_PC.c"
+#include "Resources_V15/Subroutines/Real_numbers_to_from_PC.c"
