65d2ed91a1b11a31b710b4419bd1003991030256
aclandmark <mark.osborne15@btinternet.com>
2020-05-25 17:08:38 +0100

    Update Proj_7E.ino

--- 3_UNO_projects/Proj_7_Real_numbers/Proj_7E_float_from_KBD_multiplication/Proj_7E/Proj_7E.ino
+++ 3_UNO_projects/Proj_7_Real_numbers/Proj_7E_float_from_KBD_multiplication/Proj_7E/Proj_7E.ino
@@ -40,8 +40,8 @@ long number[3];
 signed char  expnt[3];    
 char digits[8];
 
-setup_HW; 
-
+setup_UNO; 
+User_prompt;
 while(1){
 
 String_to_PC("Send real number plus cr twice.\r\n\
@@ -49,7 +49,7 @@ Press sw2 to pause display and sw1 or 3 to enter new number\r\n\
 or zero to exit\r\n");
 
 number[0] = fpn_from_KBD(digits, &expnt[0]);
-fpn_to_PC(number[0],expnt[0]);              //fpn format is +/-0._ _ _ _.... +/-E__
+fpn_to_PC(number[0],expnt[0]);                                                       //fpn format is +/-0._ _ _ _.... +/-E__
 
 while(1){
 Char_to_PC(' '); String_to_PC ("* ");
@@ -63,7 +63,7 @@ fpn_multiply_Local(number,  expnt);
 fpn_to_PC(number[2], expnt[2]);Char_to_PC('\t');
 
 I2C_Tx_float_num(number[2], expnt[2]);
-I2C_Tx_float_display_control();
+I2C_Tx_float_display_control;
 
 newline();
 number[0] = number[2]; expnt[0] = expnt[2];}}}
@@ -101,8 +101,8 @@ long fpn_reciprocal_Local(long num, signed char num_expnt, signed char *res_expn
 signed char  local_expnt[3];
 long local_num[3]; 
 
-local_num[0] = 0x0CCCCCCC;    //0.1
-local_expnt[0] = 1;       //10
+local_num[0] = 0x0CCCCCCC;                                //0.1
+local_expnt[0] = 1;                                       //10
 local_num[1] = num;
 local_expnt[1] = num_expnt;
 fpn_divide_Local(local_num, local_expnt);
