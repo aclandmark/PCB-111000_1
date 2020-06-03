b6781b11632289fcb06cad0dadd932e863e01bd9
aclandmark <mark.osborne15@btinternet.com>
2020-06-03 18:02:56 +0100

    Update Proj_2B1_AVR.c

--- 2_AVR_projects/Proj_2_Led_display_56_element_AVR/Proj_2B1_snow_storm_display_AVR/Proj_2B1_AVR.c
+++ 2_AVR_projects/Proj_2_Led_display_56_element_AVR/Proj_2B1_snow_storm_display_AVR/Proj_2B1_AVR.c
@@ -5,56 +5,42 @@ Snow storm display*******************************************/
 /*Pres sw2 to pause the display. Open serial monitor and send any char
 to change the display intensity.  (Remember to set "no line ending".)*/
 
+/*SNOW STORM DISPLAY VERSION 2
+
+In this version the random number generator is always reset to 0xFFFF 
+(1111 1111 1111 1111) and the EEPROM is not used.
+
+When the WDT times out the display continues after a short pause but 
+always restarts from the same state (i.e. all ones).
+
+Press sw_2 to operate the WDT.*/
+
+
+
+
+
 
 #include "Proj_2B1_header_file.h"
 
 
-unsigned int PRN;                                 //Global memory location used to store "pseudo random numbers"
-char Dimmer_control;
+unsigned int PRN;                             //Global memory location used to store "pseudo random numbers"
+
 
 int main (void){  
 setup_UNO_extra;
-wdt_enable(WDTO_250MS);                           //Following a WD reset the PRN is re-initialised to 0xFFFF
-
-config_sw1_and_sw2_for_PCI;                       //SW1 is not used
-
-UCSR0B |= (1 << RXCIE0);                          //Set up interrupt on key press
-Dimmer_control = 1;
+wdt_enable(WDTO_250MS);                      //Following a WD reset the PRN is re-initialised to 0xFFFF
+config_sw1_and_sw2_for_PCI
 sei();
-            
-while(1){                                         //Infinite while loop
 
-PRN = PRN_16bit_GEN (0);                          //Generate a new PRN using the previous value as input
-I2C_Tx_2_integers(PRN, (PRN<<1));                 //Display two "pseudo random numbers"
-Timer_T0_10mS_delay_x_m(10);                      //Pause before repeating
-wdr();}}                                          //Reset the watchdog timer which avoids the possibility
-                                                  //of a reset for another 250mS
+PRN = 0xFFFF;                               //Program supplied initial value fror PRN                 
+while(1){                                   //Infinite while loop
+I2C_Tx_2_integers(PRN, (PRN<<1));           //Display two "pseudo random numbers"
+PRN = PRN_16bit_GEN_UNO (PRN);                  //Generate a new PRN using the previous value as input
+Timer_T0_10mS_delay_x_m(10);                //Pause before repeating
+wdr();}}                                    //Reset the watchdog timer which avoids the possibility
+                                            //of a reset for another 250mS
 
-
-/****************************************************************************************************/
 ISR(PCINT2_vect) 
-{if (switch_2_up)return; else while(1);}          //If switch_2 is pressed put program execution on hold
-                                                  //The watchdog timer will not be reset and will "time out"
-
-
-/****************************************************************************************************/
-ISR(USART_RX_vect){
-receiveChar();
-I2C_Tx(1, 'Q', &Dimmer_control);}  
-
-
-
-
-/*Local version of subroutine "I2C_Tx()"
-
-void I2C_Tx_local(char num_bytes, char mode, char* s){
-waiting_for_I2C_master;                                       //Turn on I2C slave and await call from master
-send_byte_with_Ack(num_bytes);                                //send data byte, request acknowledgement
-send_byte_with_Ack(mode);
-for (int m = 0;  m < num_bytes; m++){
-if (m==num_bytes-1){send_byte_with_Nack(s[m]);}               //Last byte, no ackowledgement needed
-else {send_byte_with_Ack(s[m]);}}
-TWCR = (1 << TWINT);}                                         //Clear interrupt and close I2C slave*/
-
-
-                                                
+{if (switch_2_up)return; 
+else while(1);}                             //If switch_2 is pressed put program execution on hold
+                                            //The watchdog timer will not be reset and will "time out"
