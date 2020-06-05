f9c58d5743e401e54e797b8a20cb51a9d32733b4
aclandmark <mark.osborne15@btinternet.com>
2020-06-05 17:22:29 +0100

    Update Proj_2C1.ino

--- 3_UNO_projects/Favorites/Proj_2C1_random_LEDs/Proj_2C1/Proj_2C1.ino
+++ 3_UNO_projects/Favorites/Proj_2C1_random_LEDs/Proj_2C1/Proj_2C1.ino
@@ -3,11 +3,16 @@
 /**************Proj_2C1_random_LED kaleidoscope***************************
 
 The following project resources are introduced:
-  I2C_Tx_any_segment(segment, digit_num);
+  I2C_Tx_any_segment(segment, digit_num);                     This illuminates a single segment
   I2C_Tx_any_segment_clear_all();
-  PRN_16bit_GEN (0) Pseudo Random Number Generator
-    As a result of the zero PRN values are taken
-    from the EEPROM and saved to it.  
+  PRN_16bit_GEN (0) Pseudo Random Number Generator            A sequence of numbers is generated each new number dependent on
+                                                              the previous one. As a result of the zero (0)numbers are saved to eeprom.
+                                                              and read from it
+  "receiceChar()"                                             This reads the receive buffer of the UART  (receiver/transmitter module)
+
+
+Note:
+The UART receive interrupt is introduced.  Any keypress can be used to pause program flow so triggering a SW_reset.
 */
 
 
@@ -18,12 +23,12 @@ The following project resources are introduced:
 #include "Proj_2C1_header_file.h"
 
 char display_bkp[7];                                            //One element to backup each segment letter
+                                                                //Segments are labelled a,b,c,d,e,f and g
 
 int main (void){
 char segment=0, digit_num=0, seg_counter = 0,direction = 0;
 
 setup_UNO;
-
 wdt_enable(WDTO_2S);                                           //WDT prevents display from being completed in either direction
 
 I2C_Tx_any_segment_clear_all();                                //Initialise display
@@ -46,6 +51,7 @@ I2C_Tx_any_segment(segment, digit_num);                        //Update display
 backup_the_display(segment, digit_num);                        //keep backup up to date
 Timer_T0_10mS_delay_x_m(5);  wdr();                            //delay and reset watch dog
 seg_counter += 1;}
+
 direction ^= 1;                                                //Toggle the direction_counter value
 seg_counter = 0;
 Timer_T0_10mS_delay_x_m(100);}}                                 //Just pause before toggling LEDs off one at a time
@@ -61,4 +67,4 @@ display_bkp[segment - 'a'] = display_bkp[segment - 'a'] ^ (1 << digit_num);}
 /*****************************************************************/
 ISR(USART_RX_vect){receiveChar();
 I2C_Tx_any_segment_clear_all();
-sei();while(1);}
+while(1);}                                                      //Holt program flow until watch dog times out triggering a reset 
