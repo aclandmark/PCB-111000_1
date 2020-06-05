4aad34e4b6ccc8f36adb476e1c99db2e0bdf24fa
aclandmark <mark.osborne15@btinternet.com>
2020-06-05 17:22:16 +0100

    Update Proj_1C1.ino

--- 3_UNO_projects/Favorites/Proj_1C1_Target_practice/Proj_1C1/Proj_1C1.ino
+++ 3_UNO_projects/Favorites/Proj_1C1_Target_practice/Proj_1C1/Proj_1C1.ino
@@ -2,10 +2,15 @@
  Proj_1C1_Target_practice.c
  
  An attempt to introduce a bit of fun
+ Switch 2 is the gun, the leds are the targets
  
- It introduces two macros
-  config_sw2_for_PCI; (Pin Change Interrupt) used to detect switch presses
+ 
+ It also introduces two macros
+  config_sw1_and_sw2_for_PCI; (Pin Change Interrupt) used to detect switch presses
   SW_reset; This triggers a watch dog time out
+
+ Note:
+ The logic may look a bit tricky at first, but its not really what's important here.
  */
 
 
@@ -13,46 +18,42 @@
 #include "Proj_1C1_header_file.h"
 
 
-
 volatile unsigned int PORT_1, mask;
 char switch_control;
 
 int main (void){
   setup_UNO;
-  
-  
+    
   config_sw1_and_sw2_for_PCI;
   disable_pci_on_sw1
   
-  
-  
-  
-  mask = 0xFFFF;
-  switch_control = 0;
+  mask = 0xFFFF;                                  //"mask" keeps a record of which leds have been shot down
+  switch_control = 0;                             //This enables the gun when set to zero
   sei();
-  while(mask){
-    PORT_1=1;
+  while(mask){                                    //Keep looping untill all leds have been shot down
+    PORT_1=1;                                     //Start by illuminating most right hand leds
 
-    for(int m = 1; m < 17; m++){
-      if(m == 1)switch_control = 0;
-      I2C_Tx_2_integers(PORT_1 & mask, (~mask) ^ PORT_1);
+    for(int m = 1; m < 17; m++){                  //Illuminate all 16 sets of leds in turn
+      if(m == 1)switch_control = 0;               //Enable the gun
+      I2C_Tx_2_integers\
+      (PORT_1 & mask, (~mask) ^ PORT_1);          //Illumination of leds depends upon which have been shot down        
       
-      Timer_T0_10mS_delay_x_m(10);
-      PORT_1 = (PORT_1 << 1);
-    }}I2C_Tx_2_integers(0, 0xFFFF);
+      Timer_T0_10mS_delay_x_m(10);                //100mS delay
+      PORT_1 = (PORT_1 << 1);}}                   //Move on to next target and eventuallly repeat the while loop.
+      
+    I2C_Tx_2_integers(0, 0xFFFF);                 //Hold display for 1 second before restarting
     Timer_T0_10mS_delay_x_m(100);
   SW_reset;}
 
 
 
   /**************Routine executed every time that switch is  operated**************************************/
-  
-  ISR(PCINT2_vect) {                          //This ISR momentarily interrupts the main routine
-    if(switch_2_up)return;                    //It notes which LED has just been shot down and
-    if (switch_control) return;               //Return if "switch_control" is not zero
-    mask &= ~PORT_1;                          //writes zero at its location in the "mask" register
-  switch_control = 1;}
-
+                                              
+  ISR(PCINT2_vect) {                          //The ISR is called when the trigger is operated
+    if(switch_2_up)return;                    //Only switch presses are significant, switch releases are ignored
+    if (switch_control) return;               //The shot is ignored if the gun has been dissabled
+    mask &= ~PORT_1;                          //Otherwise it writes zero to the "mask" register at the target location
+  switch_control = 1;}                        //and temporarily dissables the gun
 
 
   
