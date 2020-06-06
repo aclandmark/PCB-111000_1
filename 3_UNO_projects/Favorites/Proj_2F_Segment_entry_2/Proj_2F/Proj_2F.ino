c3009c3c22c3a02b931cb8a836eb556334772e1e
aclandmark <mark.osborne15@btinternet.com>
2020-06-06 12:50:00 +0100

    Update Proj_2F.ino

--- 3_UNO_projects/Favorites/Proj_2F_Segment_entry_2/Proj_2F/Proj_2F.ino
+++ 3_UNO_projects/Favorites/Proj_2F_Segment_entry_2/Proj_2F/Proj_2F.ino
@@ -4,9 +4,29 @@
   *******************************************************************************/
 
   /*DISPLAYING NUMBERS USING MANUAL ILLUMINATION OF SEGMENTS
+
+Demonstrates one step in the process of displaying numbers 
     
-  Introduces subroutine isCharavailable()
+  It Introduces 
+  
+  1. Subroutine isCharavailable()
   This waits several mS and returns 1 if a character has been received.
+  Subroutine "receiveChar()" can then be used to acquire it
+  
+  2. Macro "User_prompt":
+  This sends the following character string to the PC "R     R      R      R........"
+  The user responds with keypress "r" or "R".
+
+  3.  Macro "SW_reset_with_interrupt".  
+      When the watch dog times out after 30mS the program is not immediatey reset.  
+      Instead an interrupt is generated. The ISR writes to EEPROM.  The reset is 
+      generated as the program exits from the ISR.
+
+  4.  Project macros 
+      "set_Arduino_WDTout"        This writes to the EEPROM when the WDT ISR is called
+      "Arduino_non_WDTout"        This reads the EEPROM to determine whether or not the reset is due to a WDTimeout.      
+      "clear_Arduino_WDT_flag"    This clears the EEPROM
+      
   
   Having downloaded the program start up a terminal program and pres 'r' at the user prompt
   
@@ -25,21 +45,16 @@
 
   setup_UNO;
 
+  if (Arduino_non_WDTout)                                 //Reset due to POR or immediately after being programmed
+  {User_prompt;                                           //Display the user prompt and
+   String_to_PC(message_1);}                              //full start-up message
+  
+  else {clear_Arduino_WDT_flag;                          //Reset due to watch dog time out 
+  newline();String_to_PC(message_2);                      //Send abbreviated message and no user prompt
+  
+  I2C_Tx_any_segment('h', 0);}                          //Restore display after reset
 
-  //User_prompt;
-  
-  //if (!(watch_dog_reset)) ////////////////////////////  //Normal start up i.e. POR or post programming
-  
-  
-  if (Arduino_non_WDTout)
-  {User_prompt;
-   String_to_PC(message_1);}                              //Send normal start-up message
-  
-  else {clear_Arduino_WDT_flag;                            //Start up following SW_reset (reset watch_dog_reset flag)
-  newline();String_to_PC(message_2);                    //Send abbreviated message
-  I2C_Tx_any_segment('h', 0);}                          //Restore display after SW_reset
-
-  do{                                                   //Keep inputting data until x is received
+  do{                                                   //Keep inputting segment/digit data until x is received
   while(1){if(isCharavailable(10))
   {keypress = receiveChar();break;}}                   //"isCharavailable" is repeatedly executed until a character is received
                                                         //when the "break" command allows program flow to escape from the "while(1)" loop
@@ -48,23 +63,20 @@
   case '0': case '1': case '2':
   case '3': case '4': case '5':
   case '6': case '7': 
-  digit_num = keypress - '0'; break;                     //If numeric save it as "digit_num".
+  digit_num = keypress - '0'; break;                    //If numeric save it as "digit_num".
 
   case 'a': case 'b': case 'c':                         //If a letter illuminate the display
   case 'd': case 'e': case 'f':
   case 'g': 
   I2C_Tx_any_segment(keypress, digit_num); break;
 
-  case 'x': break;                                      //if x clear the display
-  default: {SW_reset;} break;}                          //if any other key (AOK) execute a SW_reset.
+  case 'x': break;                                      //Use "x" to start again
+  default: {SW_reset_with_interrupt;} break;}           //Any other key (AOK) press is ignored.
                                                         //Bottom of switch block return to top of "do-loop"
   }while(keypress != 'x');                              //Bottom of "do-loop" with exit condition
 
-  I2C_Tx_any_segment_clear_all();
-  SW_reset_with_interrupt;}                                            //Repeat program
-
-
-
+  I2C_Tx_any_segment_clear_all();                       //Clear display and
+  SW_reset_with_interrupt;}                             //repeat program
 
 
 
