e718ae3f91481c0276213f48f7f199955a4c9f49
aclandmark <mark.osborne15@btinternet.com>
2020-06-07 16:39:18 +0100

    Update Proj_2C1_AVR.c

--- 2_AVR_projects/Proj_2_Led_display_56_element_AVR/Proj_2C1_random_LEDs_AVR/Proj_2C1_AVR.c
+++ 2_AVR_projects/Proj_2_Led_display_56_element_AVR/Proj_2C1_random_LEDs_AVR/Proj_2C1_AVR.c
@@ -47,4 +47,5 @@ display_bkp[segment - 'a'] = display_bkp[segment - 'a'] ^ (1 << digit_num);}
 /*****************************************************************/
 ISR(USART_RX_vect){receiveChar();
 I2C_Tx_any_segment_clear_all();
-sei();while(1);}
+//sei();
+while(1);}
