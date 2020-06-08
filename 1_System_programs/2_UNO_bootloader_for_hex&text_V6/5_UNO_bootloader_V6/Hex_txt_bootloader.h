4d1c37c5ad10f7f46f348ca9de7a0346f0f550a9
aclandmark <mark.osborne15@btinternet.com>
2020-06-08 18:26:34 +0100

    Update Hex_txt_bootloader.h

--- 1_System_programs/2_UNO_bootloader_for_hex&text_V6/5_UNO_bootloader_V6/Hex_txt_bootloader.h
+++ 1_System_programs/2_UNO_bootloader_for_hex&text_V6/5_UNO_bootloader_V6/Hex_txt_bootloader.h
@@ -8,6 +8,6 @@
 
 
 
-#include "../6_Resources\Hex_txt_bootloader.h"
-#include "../6_Resources\Hex_txt_bootloader_SW_subs.c"
-#include "../6_Resources\Basic_IO_subroutines.c"
+#include "../6_Resources_UNO\Hex_txt_bootloader.h"
+#include "../6_Resources_UNO\Hex_txt_bootloader_SW_subs.c"
+#include "../6_Resources_UNO\Basic_IO_subroutines.c"
