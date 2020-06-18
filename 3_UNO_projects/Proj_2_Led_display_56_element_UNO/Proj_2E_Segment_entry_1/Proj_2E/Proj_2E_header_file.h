98230962893ffbf38caeafd47c5161e5fa43b10f
aclandmark <mark.osborne15@btinternet.com>
2020-06-18 17:33:43 +0100

    Update Proj_2E_header_file.h

--- 3_UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2E_Segment_entry_1/Proj_2E/Proj_2E_header_file.h
+++ 3_UNO_projects/Proj_2_Led_display_56_element_UNO/Proj_2E_Segment_entry_1/Proj_2E/Proj_2E_header_file.h
@@ -8,10 +8,10 @@
 #include "Resources_V15/Header_files/I_O_from_switches_UNO.h"
 #include "Resources_V15/Subroutines/Data_to_from_EEPROMs.c"
 
+#define Thirty_mS_watch_dog_with_interrupt \
+wdr();\
+WDTCSR |= (1 <<WDCE) | (1<< WDE);\
+WDTCSR = (1<< WDE) | (1 << WDIE) |  (1 << WDP0);
 
-/*
-#include "../../Resources_V12/Subroutines/Random_and_prime_nos.c"
-#include "../../Resources_V12/I2C_Subroutines/I2C_diagnostic.c"
-*/
 
 char isCharavailable_Local (char);
