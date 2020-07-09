d23885169209aae24187afb4ef26e41c88ec3a3e
aclandmark <mark.osborne15@btinternet.com>
2020-07-09 08:54:07 +0100

    Update Proj_1A1_AVR.c

--- 2_AVR_projects/Favorites/Proj_1A1_LED_display_basic_AVR/Proj_1A1_AVR.c
+++ 2_AVR_projects/Favorites/Proj_1A1_LED_display_basic_AVR/Proj_1A1_AVR.c
@@ -1,25 +1,28 @@
 
+
+//Proj_1A1:  A Basic first sample program
+
+
 #include "Proj_1A1_header_file.h"
 
 
 
 
-int main (void){								//The "main" routine lies between a pair of {} brackets.
+int main (void){								//When the program runs it always starts with the "main" routine. 
 
-unsigned long PORT_1, PORT_2;
+unsigned long PORT_1, PORT_2;					//Reserve two addresses in memory and give then the names PORT_1 and PORT_2
  
 
-setup_UNO_extra;					
+setup_UNO_extra;								//Basic UNO device configuration: the same for all these projects 					
 
-while(1)
-{PORT_1=1; PORT_2 = 0x80000000;					//1000 0000 0000 0000 0000 0000 0000 0000
+while(1)										//An infinite loop follows
+{PORT_1=1; PORT_2 = 0x80000000;					//0x80000000 is the same as 1000 0000 0000 0000 0000 0000 0000 0000 in binary
 
-	for(int m = 1; m < 33; m++){				//Code between the {} brackets is repeated 16 times 						
-												//as m increments from 1 to 16	(m++ is shorthand for increment m)
-		I2C_Tx_2_integers(PORT_1, PORT_2);				
+	for(int m = 1; m <= 32; m++){				//Code between the {} brackets is repeated 32 times 						
+		I2C_Tx_2_integers(PORT_1, PORT_2);		//Use the values of PORT_1 and PORT_2 to drive the LED display			
 
-		Timer_T1_sub(T1_delay_50ms);			//This subroutine generates a 10mS delay that is repeated 6 times		
-		PORT_1 = PORT_1 << 1;					//The contents of address "PORT_1" are shifted one place left
-		PORT_2 = PORT_2 >> 1;
+		Timer_T0_10mS_delay_x_m(6);				//This subroutine generates a 10mS delay that is repeated 6 times		
+		PORT_1 = PORT_1 << 1;					//The contents of address "PORT_1" are shifted one place left and
+		PORT_2 = PORT_2 >> 1;					//the contents of address "PORT_2" are shifted one place right 
 		}										//every time that the "for-loop" is executed
 }}
