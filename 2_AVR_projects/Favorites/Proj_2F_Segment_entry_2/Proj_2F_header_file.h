5fc63b35fee4492db74480412ba652647a84c70f
aclandmark <mark.osborne15@btinternet.com>
2020-07-09 08:53:30 +0100

    Update Proj_2F_header_file.h

--- 2_AVR_projects/Favorites/Proj_2F_Segment_entry_2/Proj_2F_header_file.h
+++ 2_AVR_projects/Favorites/Proj_2F_Segment_entry_2/Proj_2F_header_file.h
@@ -5,3 +5,4 @@
 #include "../../Resources_V15/I2C_Subroutines/I2C_subroutines_1.c"
 #include "../../Resources_V15/Subroutines/Basic_PC_comms.c"
 #include "../../Resources_V15/I2C_Subroutines/I2C_slave_Rx_Tx.c"
+#include "../../Resources_V15/Header_files/I_O_from_switches_UNO.h"
