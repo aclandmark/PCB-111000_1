e4ba97a1ef1c934bb9bf29c3402da12be4911c3e
aclandmark <mark.osborne15@btinternet.com>
2020-07-09 08:53:38 +0100

    Update Proj_2F.c

--- 2_AVR_projects/Favorites/Proj_2F_Segment_entry_2/Proj_2F.c
+++ 2_AVR_projects/Favorites/Proj_2F_Segment_entry_2/Proj_2F.c
@@ -30,16 +30,11 @@ introduce unnecessary complexity.
 Note "SW_reset" works by enabling the WD timer and letting it time out.
 
 4.	The "#define" statement used to define text for use with the “String_to_PC()” subroutine.
-
-5.	“&&” a logical operation.
-For example if(A && B){do this} which means
-if (expression A is not zero and expression B is not zero) {do this}
-But if one or both of these expressions equates to zero continue without doing it. 
-
-6.	The "do{program segment}while(condition);" loop
-which keep repeating the program segment as long as the condition is non-zero.*/
+*/
 
 
+
+//Proj_2F:  Display numbers manually
 
 
 #include "Proj_2F_header_file.h"
@@ -47,8 +42,10 @@ which keep repeating the program segment as long as the condition is non-zero.*/
 
 
 
-#define message_1 "For each digit press 0 to 7 then a to g for segments."
-#define message_2 "Again!"
+#define message_1 "\r\nPress a to g for segments, then x for next digit.\r\n\
+Press y or switch 1 for SW reset.\r\n"
+
+#define message_2 "SW_reset?"
 
 
 
@@ -59,36 +56,59 @@ int main (void){
 char keypress = 0, digit_num=0;						
 
 setup_UNO_extra;
+config_sw1_and_sw2_for_PCI;
+
 
 if (!(watch_dog_reset)) 												//Normal start up i.e. POR or post programming
 String_to_PC(message_1);												//Send normal start-up message
 
 
-else {watch_dog_reset = 0;												//Start up following SW_reset (reset watch_dog_reset flag)
-newline();String_to_PC(message_2);										//Send abbreviated message						
-I2C_Tx_any_segment('h', 0);}											//Restore display after SW_reset
+else {watch_dog_reset = 0;												//Start up following SW_reset (reset the watch_dog_reset flag)
+newline();String_to_PC(message_2);}									//Send abreviated message						
 
-do{																		//Keep inputting data until x is received
 
-while(1){if(isCharavailable(10)){keypress = receiveChar();break;}}	//"isCharavailable" is repeatedly executed until a character is received
-																		// when the "break" command allows program flow to escape from the "while(1)" loop
+sei();
+wdt_enable(WDTO_30MS);													//Note: Start WDT
+
+
+do{																		//Keep inputting data until "y" is received
+
+while(1){if(isCharavailable(10)){keypress = receiveChar();break;}}	//"isCharavailable()" resets the WDT everytime that it is called
 
 switch(keypress){														//Test keypress
-case '0': case '1': case '2': 
-case '3': case '4': case '5': 
-case '6': case '7': digit_num = keypress - '0'; break;					//If numeric save it as "digit_num".
 
 case 'a': case 'b': case 'c':											//If a letter illuminate the display 
 case 'd': case 'e': case 'f': 
 case 'g': I2C_Tx_any_segment(keypress, digit_num); break;
 
-case 'x': break;														//if x clear the display
+case 'x': digit_num = (digit_num + 1)%8;								//If "x" increment digit_num
 
-default: {SW_reset;} break;												//if any other key (AOK) execute a SW_reset.
+default: break;															//Ignore AOK press
 }																		//Bottom of switch block return to top of "do-loop"
-}while(keypress != 'x');												//Bottom of "do-loop" with exit condition
+}while(keypress != 'y');												//Bottom of "do-loop" with exit condition
 
-I2C_Tx_any_segment_clear_all();
-SW_reset;}																//Repeat program
+
+while(1);}																//WDT times out
+
+
+
+
+ISR(PCINT2_vect) 														//SWitch press
+{if (switch_1_down) 													//clears display before executing a  reset
+{I2C_Tx_any_segment_clear_all();while(1);}}
+
+
+
+
+char isCharavailable_Local (char m)
+{int n = 0;
+while (!(UCSR0A & (1 << RXC0)))										//If a key press is not detected
+{n++;	if (n>8000) 													//Increment "n" from zero to 8000
+{m--;n = 0;															//then reset it, decrement m
+wdr();}																	//and reset WDT to avoid an unwanted reset
+if (m == 0)return 0;}													//Returns a 0 if no keypress is detected
+return 1;}																//Exit imediately and return 1 when a keypress is detected
+
+
 
 
