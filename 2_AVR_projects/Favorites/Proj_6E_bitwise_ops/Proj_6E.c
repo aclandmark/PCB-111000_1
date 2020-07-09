ec8407eecc064506f2719ca7fdfcd429872ec511
aclandmark <mark.osborne15@btinternet.com>
2020-07-09 08:52:27 +0100

    Update Proj_6E.c

--- 2_AVR_projects/Favorites/Proj_6E_bitwise_ops/Proj_6E.c
+++ 2_AVR_projects/Favorites/Proj_6E_bitwise_ops/Proj_6E.c
@@ -22,100 +22,146 @@ These options give 4 modes of operation.*/
 
 #include "Proj_6E_header_file.h"
 
+char Op(char, char);
+
+
+char digits[8];
+char BWOp, comp;	
 
 int main (void){
 char op_code;
-char digits[8], X;
+char X;
 char User_response, keypress;
 
 setup_UNO_extra;
 User_instructions;
 User_prompt;
 
+
+
+
+switch (User_response)
+{ case 'R':
+
 String_to_PC("\r\nSelect logical operation \r\n\
 1 for << X	Shifts a number left\r\n\
 2 for >> X	Shifts a number right \r\n");
 String_to_PC("\
-3 for | (1 << X)	Changes just one bit by setting it to 1.\r\n\
-4 for &(~(1 << X)	Changes just one bit by setting it to 0.\r\n");
+3 for << X	Rotate a number left\r\n\
+4 for << X	Rotate a number right\r\n");
 String_to_PC("\
-5 for XOR(1 << X)	Changes just one bit by reversing it.\r\n\
-6 for &(1 << X)	Tests a single bit\r\n");
+5 for | (1 << X)	Changes just one bit by setting it to 1.\r\n\
+6 for &(~(1 << X)	Changes just one bit by setting it to 0.\r\n");
+String_to_PC("\
+7 for XOR(1 << X)	Changes just one bit by reversing it.\r\n\
+8 for &(1 << X)	Tests a single bit\r\n");
 
 waitforchar();
 while(1){
 op_code = receiveChar();	 	
 
 
-		switch(op_code){
+	switch(op_code){
 		case '1':
-		case '2': String_to_PC("X? 1-7\t"); X = waitforkeypress()-'0'; break;
+		case '2':
 		case '3':
-		case '4':
+		case '4': String_to_PC("X? 1-7\t"); X = waitforkeypress()-'0'; break;
+		
+		
 		case '5':
-		case '6': X = 1;  break;
+		case '6':
+		case '7':
+		case '8': X = 1;  break;
 		default: wdt_enable(WDTO_30MS); while(1); break;}			//Exit program
 		
-		
-		switch(User_response){
-		case 'R': String_to_PC("Enter binary number: 1 to 0b11111111 terminate in cr then AK  "); 
-		dig_0 = binary_char_from_KBD_Local(); break;
-		case 'r': String_to_PC("Random number used: AK   "); 
+		String_to_PC("Random number used: AK   "); 
 		dig_0 = PRN_8bit_GEN();
-		break;	}
-		
-		dig_1 = X;
-		dig_2 = 0;
-				
+		dig_1 = X ;
+		//dig_2 = 0;
+		dig_2 = dig_0;
+
 		do{
 		I2C_Tx_BWops(digits);										//update the display
 		keypress=waitforkeypress();
-		dig_2 = logical_op(X,dig_0, op_code, User_response); 		//perform logical op and update dig_2
+		dig_2 = logical_op(X,dig_0, op_code); 		//perform logical op and update dig_2
+		
 		switch (op_code){
-		case '1': 
-		case '2': X = dig_1 + X;break;								//update X for op_codes 1 and 2
+		case '1':
+		case '2':		
+		case '3': 
+		case '4': X += dig_1; break;								//update X for op_codes 1 and 2
 		default: dig_1 = X; X = (X <<1)%255;	break;}				//update X and dig_1 for op-codes 3-6
 		}while((keypress != 'x') && (keypress != 'X'));
 		
+		I2C_Tx_any_segment_clear_all();
 		if(keypress == 'X'){wdt_enable(WDTO_30MS); while(1);}
 		String_to_PC("New mode?\r\n");
-		waitforchar();}} 
+		waitforchar();}break;	
+		
+		
+		
+		
+		
+		
+	case 'r':	
+		String_to_PC("Select OP: 	| 	^ 	& 	~| 	~^ 	or	~&");
+BWOp = waitforkeypress();
+if (BWOp == '~') {comp = 1; BWOp = waitforkeypress();}else comp = 0;				//detect complement operator
+if ((BWOp != '|') && (BWOp != '^') && (BWOp != '&'))
+{wdt_enable(WDTO_30MS); while(1);}													//reset if duff char was sent 
+	
+
+String_to_PC("\r\nUse random numbers: Press AK or x to escape\r\n");				//Operates on random data
+while (waitforkeypress() !='x'){
+digits[0] = PRN_8bit_GEN();
+digits[1] = PRN_8bit_GEN();
+digits[2] =  Op(digits[0] , digits[1]);
+I2C_Tx_BWops(digits);}									
+
+I2C_Tx_any_segment_clear_all();
+
+wdt_enable(WDTO_30MS); while(1);break;}
+		
+		
+		
+		} 
 
 
 
 /*************************************************************************************/	
-char logical_op(char X, char Y, char op_code, char mode){
+char logical_op(char X, char Y, char op_code){
 char result = 0, n = 0, num;
 switch (op_code){
 
-case '1': if(mode == 'r'){Rotate_Right_cyclical; 
-result = Y;}else {result = Y << X;} break;
-case '2': if(mode == 'r'){Rotate_left_cyclical; 
-result = Y;} else {result = Y >> X;}break;
 
-case '3': result = Y | X; break;
-case '4': result = Y &(~(X)); break;
-case '5': result = Y ^ (X); break;
-case '6': result = Y & (X); break;}
+case '1': result = Y << X;break;
+case '2': result = Y >> X;break;
+
+//case '1': {Rotate_Right_cyclical; result = Y;}break;
+//case '2': if(mode == 'r'){Rotate_left_cyclical; 
+//result = Y;} else {result = Y >> X;}break;
+
+
+
+case '3': Rotate_Right_cyclical; result = Y; break;
+case '4': Rotate_left_cyclical; result = Y; break;
+
+
+
+case '5': result = Y | X; break;
+case '6': result = Y &(~(X)); break;
+case '7': result = Y ^ (X); break;
+case '8': result = Y & (X); break;}
 return result;}
 
 
 
-/*************************************************************************************/
-char binary_char_from_KBD_Local(void){
-char keypress=0, LSB, array[8];
-
-for(int m = 0; m<=7; m++)array[m]=0;
-do{
-LSB = wait_for_return_key(); 
-if (LSB == '\r')break;
-if((LSB != '0') && (LSB != '1'));
-
-else {keypress = (keypress << 1) + LSB - '0';
-array[0] = keypress;
-I2C_Tx_BWops(array);
-}} while(1);
-return keypress;}
-
-
+char Op(char A, char B)
+{char result=0;
+switch (BWOp){
+case '|': result = A | B; break;
+case '^': result = A ^ B; break;
+case '&': result = A & B; break;}
+if (comp == 1) result = ~result;
+return result;}
 
