429ada8a813752fc26355e7f3d174c85307e583b
aclandmark <mark.osborne15@btinternet.com>
2020-07-09 08:52:59 +0100

    Update Proj_3G_AVR.c

--- 2_AVR_projects/Favorites/Proj_3G_Char_nos_AVR/Proj_3G_AVR.c
+++ 2_AVR_projects/Favorites/Proj_3G_Char_nos_AVR/Proj_3G_AVR.c
@@ -2,20 +2,29 @@
 /*Press middle switch to increment display, sw1 to decrement it and sw3 to change the sign*/
 
 
+//Proj_3G:  Demonstates positive and negative binary numbers 
+
+
+
+
 #include "Proj_3G_header_file.h"
 
 char digit=1, op, mode;
+
 
 int main (void){
 
 op=0;
 mode = 'u';                                               //mode: signed or unsigned
+
 setup_UNO_extra;
+
 I2C_Tx_display_char(digit,mode);
 setup_and_enable_PCI;
 sei();
 
 while(1){
+
 switch(op){
 case 0: break;                                            //Static display
 case 1: I2C_Tx_display_char(++digit,mode);break;          //Increment "digit" before calling "I2C_Tx....."
@@ -24,13 +33,15 @@ case 2: I2C_Tx_display_char(--digit, mode);break;}        //Decrement "digit" be
 Timer_T0_10mS_delay_x_m(15);}}
 
 
+
 /**************************************************************************************************/
 ISR(PCINT2_vect) {                                        //sw1 and sw2 interrupt
 if(switch_3_down)return;                                  //Ignore if sw3 is still down
-if((switch_2_up) && (switch_1_up)){op = 0; return;}       //Both switches up
+if((switch_2_up) && (switch_1_up)){op = 0; return;}      //Both switches up
 
 if(switch_2_down) {op = 1; return;}                       //Digits increment
 if(switch_1_down) {op = 2; return;}}                      //Digits decrement
+
 
 
 /**************************************************************************************************/
@@ -41,3 +52,8 @@ if (mode == 's')
 {mode = 'u'; I2C_Tx_display_char(digit,mode);return;}      //toggle display from signed to unsigned
 if (mode == 'u')
 {mode = 's'; I2C_Tx_display_char(digit,mode);return;}}     //and visa-versa
+
+
+
+
+
