858393e14b956442d11773f9fa3b3fe7b31f9b9b
aclandmark <mark.osborne15@btinternet.com>
2020-07-25 18:03:14 +0100

    Create UNO_programmer.h

---
+++ 8_UNO_programmer/UNO_programmer/UNO_programmer.h
@@ -0,0 +1,17 @@
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+#include "Resources_UNO_programmer\ATMEGA_Programmer.h"
+#include "Resources_UNO_programmer\ATMEGA_Programmer_HW_subs.c"
+#include "Resources_UNO_programmer\Basic_IO_and_Timer_subs.c"
+#include "Resources_UNO_programmer\ASKII_subroutines.c"
+#include "Resources_UNO_programmer\Hex_Programmer_subs.c"
+#include "Resources_UNO_programmer\EEPROM_programmer_sub.c"
+#include "Resources_UNO_programmer\Text_programmer_subs.c"
+#include "Resources_UNO_programmer\Hex_Programmer.c"
