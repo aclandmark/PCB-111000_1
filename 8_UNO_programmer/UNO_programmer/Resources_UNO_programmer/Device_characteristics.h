942a870d86efa2eca4635d811fb1634803625621
aclandmark <mark.osborne15@btinternet.com>
2020-07-25 21:25:03 +0100

    Create Device_characteristics.h

---
+++ 8_UNO_programmer/UNO_programmer/Resources_UNO_programmer/Device_characteristics.h
@@ -0,0 +1,21 @@
+
+
+
+/*
+#define Device "328P"
+#define PageSZ 0x40 
+#define PAmask 0x3FC0 
+#define FlashSZ 0x4000
+#define EE_size 0x400
+#define target_type 0x0F
+#define target_type_M 0x95
+*/
+
+
+#define Device "328"
+#define PageSZ 0x40 
+#define PAmask 0x3FC0 
+#define FlashSZ 0x4000
+#define EE_size 0x400
+#define target_type 0x14
+#define target_type_M 0x95
