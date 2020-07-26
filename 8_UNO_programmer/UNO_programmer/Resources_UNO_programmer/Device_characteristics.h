4cf6628744e19b6da7055a14d2ce4c49000c786b
aclandmark <mark.osborne15@btinternet.com>
2020-07-26 10:42:44 +0100

    Update Device_characteristics.h

--- 8_UNO_programmer/UNO_programmer/Resources_UNO_programmer/Device_characteristics.h
+++ 8_UNO_programmer/UNO_programmer/Resources_UNO_programmer/Device_characteristics.h
@@ -1,5 +1,22 @@
 
 
+#define Device "328"
+#define PageSZ 0x40 
+#define PAmask 0x3FC0 
+#define FlashSZ 0x4000
+#define EE_size 0x400
+#define target_type 0x14
+#define target_type_M 0x95
+#define  Fuse_Ex 0xFF
+#define  Fuse_H 0xD7
+#define  Fuse_L 0xE2
+#define  Lock 0xFF
+
+
+
+
+
+
 
 /*
 #define Device "328P"
@@ -9,7 +26,11 @@
 #define EE_size 0x400
 #define target_type 0x0F
 #define target_type_M 0x95
-*/
+#define  Fuse_Ex FF
+#define  Fuse_H D7
+#define  Fuse_L E2
+#define  Lock FF
+
 
 
 #define Device "328"
@@ -19,3 +40,8 @@
 #define EE_size 0x400
 #define target_type 0x14
 #define target_type_M 0x95
+#define  Fuse_Ex 0xFF
+#define  Fuse_H 0xD7
+#define  Fuse_L 0xE2
+#define  Lock 0xFF
+*/
