1dd84c5174402ad0107c6e5e1e3b528bf9963c91
aclandmark <mark.osborne15@btinternet.com>
2020-07-27 15:38:13 +0100

    Create Cal_Atmega_V1_header_file.h

---
+++ 9_cal_auto_plus_manual/Cal_Atmega_V1/Cal_Atmega_V1_header_file.h
@@ -0,0 +1,26 @@
+
+
+#include "../Cal_resources/Basic_HW_setup.h"
+#include "../Cal_resources/Basic_HW_plus_Timer.c"
+#include "../Cal_resources/Basic_PC_comms.c"
+
+
+
+#define Get_ready_to_calibrate; \
+TIMSK2 |= (1 << TOIE2);\
+TIMSK1 |= (1 << TOIE1);\
+initialise_timers_for_cal_error();\
+start_timers_for_cal_error();
+
+
+#define close_calibration; \
+initialise_timers_for_cal_error();\
+TIMSK2 &= (~(1 << TOIE2));\
+TIMSK1 &= (~(1 << TOIE1));
+
+#define calibrate_without_sign_plus_warm_up_time; \
+cal_mode = 5;\
+cal_error = compute_error(0,cal_mode,0);\
+cal_error = compute_error(0,cal_mode,0);\
+cal_error = compute_error(0,cal_mode,0);
+
