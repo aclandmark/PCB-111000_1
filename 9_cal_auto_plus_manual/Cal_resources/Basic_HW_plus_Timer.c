5e4fe33ff899108ae7c54320125d4e50a198928e
aclandmark <mark.osborne15@btinternet.com>
2020-07-30 17:14:01 +0100

    Update Basic_HW_plus_Timer.c

--- 9_cal_auto_plus_manual/Cal_resources/Basic_HW_plus_Timer.c
+++ 9_cal_auto_plus_manual/Cal_resources/Basic_HW_plus_Timer.c
@@ -1,12 +1,11 @@
+
+
 void USART_init (unsigned char, unsigned char);
 void nop_delay(int);
 void Timer_T0_10mS_delay_x_m(int);
 void Timer_T0_sub(char, unsigned char);
-void Timer_T0_sub_with_interrupt(char, unsigned char);
-void T1_65ms_clock_tick(void);
-void Timer_T1_sub_with_interrupt(char, unsigned int);
 void Timer_T1_sub(char, unsigned int);
-void Timer_T2_sub(char, char);
+
 
 
 
@@ -36,17 +35,6 @@ TIFR0 |= (1<<TOV0); TCCR0B = 0;}
 
 
 /*********************************************************************/
-void Timer_T0_sub_with_interrupt(char Counter_speed, unsigned char Start_point){ 
-TIMSK0 |= (1 << TOIE0);
-TCNT0 = Start_point;
-TCCR0B = Counter_speed;}
-
-
-/*********************************************************************/
-void T1_65ms_clock_tick(void){ 
-TIMSK1 |= (1 << TOIE1);TCCR1B = 0x02;}
-
-/*********************************************************************/
 void Timer_T1_sub(char Counter_speed, unsigned int Start_point){ 
 TCNT1H = (Start_point >> 8);
 TCNT1L = Start_point & 0x00FF;
@@ -56,20 +44,5 @@ while(!(TIFR1 & (1<<TOV1)));
 TIFR1 |= (1<<TOV1); 
 TCCR1B = 0;}
 
-/*********************************************************************/
-void Timer_T1_sub_with_interrupt(char Counter_speed, unsigned int Start_point){ 
-TIMSK1 |= (1 << TOIE1);
-TCNT1H = (Start_point >> 8);
-TCNT1L = Start_point & 0x00FF;
-TCCR1B = Counter_speed;}
 
-
-/*********************************************************************/
-void Timer_T2_sub(char Counter_speed, char Start_point){ 
-TCNT2 = (Start_point );
-
-TCCR2B = Counter_speed;
-while(!(TIFR2 & (1<<TOV2)));
-TIFR2 |= (1<<TOV2); 
-TCCR2B = 0;}
 
