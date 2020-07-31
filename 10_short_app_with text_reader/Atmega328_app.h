fbf17273084e7c546ca9f653fca2b2bcdd9f866e
aclandmark <mark.osborne15@btinternet.com>
2020-07-31 16:33:02 +0100

    Update Atmega328_app.h

--- 10_short_app_with text_reader/Atmega328_app.h
+++ 10_short_app_with text_reader/Atmega328_app.h
@@ -10,3 +10,13 @@
 
 #include "Resources\Atmega328_app.h"
 #include "Resources\Basic_IO_and_timer_subs.c"
+
+
+
+const char * Device_95 = "328/P";
+const char * Device_94 = "168/P";
+const char * Device_93 = "88/P";
+const char * Device_92 = "48/P";
+
+const char * Device_ptr[4];
+int Device;
