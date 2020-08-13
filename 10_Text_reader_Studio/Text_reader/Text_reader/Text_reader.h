8687cbd18959e39ebbb88af630271624f780b6c6
aclandmark <mark.osborne15@btinternet.com>
2020-08-13 17:25:09 +0100

    Create Text_reader.h

---
+++ 10_Text_reader_Studio/Text_reader/Text_reader/Text_reader.h
@@ -0,0 +1,13 @@
+
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/eeprom.h>
+
+
+
+#include "..\Resources\Text_reader.h"
+#include "..\Resources\Basic_IO_subs.c"
+
