cea15a2a631f39486e962917614a214b8536961f
aclandmark <mark.osborne15@btinternet.com>
2020-08-13 17:25:31 +0100

    Update Device_characteristics.c

--- 8_UNO_programmer/UNO_programmer/Resources_UNO_programmer/Device_characteristics.c
+++ 8_UNO_programmer/UNO_programmer/Resources_UNO_programmer/Device_characteristics.c
@@ -2,32 +2,30 @@
 
 
 void set_up_target_parameters(void){
-if((Device[0] == '3') && (Device[1] == '2')
-&& (Device[2] == '8'))
+if(Target_type == 0x95)									//Atmega 328/P
 {
 PageSZ = 0x40;
 PAmask = 0x3FC0;
 FlashSZ = 0x4000;
 EE_size = 0x400;
-target_type_M = 0x95;
-target_type = 0x14;
-target_type_P = 0x0F;
+//Target_type = 0x95;
+Target_type_M = 0x14;
+Target_type_P = 0x0F;
 Fuse_Ex = 0xFF;
 Fuse_H = 0xD7;
 Fuse_L = 0xE2;
 Lock = 0xFF;}
 
 
-if((Device[0] == '1') && (Device[1] == '6')
-&& (Device[2] == '8'))
+if(Target_type == 0x94)									//Atmega 168/P
 {
 PageSZ = 0x40;			
 PAmask = 0x1FC0;		
 FlashSZ = 0x2000;		
 EE_size = 0x200;		
-target_type_M = 0x94;	
-target_type = 0x6;		
-target_type_P = 0x0B;	
+//Target_type = 0x94;	
+Target_type_M = 0x6;		
+Target_type_P = 0x0B;	
 Fuse_Ex = 0xFF;
 Fuse_H = 0xD7;
 Fuse_L = 0xE2;
