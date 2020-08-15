158fd4b78bea182ae73e08caeecc23ce51d6df8a
aclandmark <mark.osborne15@btinternet.com>
2020-08-15 17:31:50 +0100

    Update Basic_IO_subs.c

--- 10_Text_reader_Studio/Text_reader/Resources/Basic_IO_subs.c
+++ 10_Text_reader_Studio/Text_reader/Resources/Basic_IO_subs.c
@@ -1,20 +1,19 @@
 
-
 void USART_init (unsigned char, unsigned char);
-void sendString(const char*);
-char isCharavailable (int);
-char receiveChar(void);
-void sendChar(char);
 char waitforkeypress(void);
-void newline(void);
-
-int Num_from_KBD(void);
+char receiveChar(void);
+char isCharavailable (int);
 char wait_for_return_key(void);
-char decimal_digit (char);
-
-void Num_to_PC(char, int);
-void SBtoAL(char*, int, char);
+void sendChar(char);
+void sendString(const char*);
+void newline(void);
+void Num_to_PC(char, long);
+void SBtoAL(char*, long, char);
 void NumericString_to_PC(char* );
+char decimal_digit (char);
+int Num_from_KBD(void);
+
+
 
 
 
@@ -29,12 +28,20 @@ UCSR0C =  (1 << UCSZ00)| (1 << UCSZ01);}
 
 
 
+
 /*********************************************************************************************/
-void sendString(const char s[]){
-int i = 0;
-while(i < 200){
-if(s[i] == '\0') break;
-sendChar(s[i++]);} }
+char waitforkeypress(void){
+while (!(UCSR0A & (1 << RXC0)));
+return UDR0;}
+
+
+
+
+/*********************************************************************************************/
+char receiveChar(void)
+{return UDR0;}
+
+
 
 
 /*********************************************************************************************/
@@ -44,9 +51,17 @@ if (n>4000) {m--;n = 0;}if (m == 0)return 0;}
 return 1;}
 
 
-/*********************************************************************************************/
-char receiveChar(void)
-{return UDR0;}
+
+
+/***************************************************************************************************************************************************/
+char wait_for_return_key(void){	
+char keypress;
+keypress = waitforkeypress();
+if((keypress == '\r') || (keypress == '\n')){
+if (isCharavailable(1)){receiveChar();}keypress = '\r';}
+return keypress;}
+
+
 
 
 
@@ -57,15 +72,73 @@ UDR0 = data;}
 
 
 
+
 /*********************************************************************************************/
-char waitforkeypress(void){
-while (!(UCSR0A & (1 << RXC0)));
-return UDR0;}
+void sendString(const char s[]){
+int i = 0;
+while(i < 200){
+if(s[i] == '\0') break;
+sendChar(s[i++]);} }
+
+
 
 
 /*********************************************************************************************/
 void newline(void){sendString ("\r\n");}
 
+
+
+
+/*********************************************************************/
+void Num_to_PC(char radix, long int_num){
+char array[12];												//long has 10 chars + sign + null terminator	
+SBtoAL(array, int_num, radix);								//calls the Binary to askii subroutine
+NumericString_to_PC(array);}								//Prints characters in reverse order
+
+
+
+
+/*********************************************************************/
+void SBtoAL(char array[], long num, char radix){					//Signed Binary to Askii
+int m=0;
+long sign;
+
+if (num == 0x80000000){
+switch(radix){
+case 10: array[0] = '8';array[1] = '4'; array[2] = '6';			//0x80000000 * -1 = 0
+array[3] = '3';array[4] = '8';array[5] = '4'; array[6] = '7';
+array[7] = '4';array[8] = '1';array[9] = '2';
+array[10] = '-'; array[11] = '\0'; break;
+
+case 16: array[0] = '0';array[1] = '0'; array[2] = '0';			//0x80000000 * -1 = 0
+array[3] = '0';array[4] = '0';array[5] = '0'; array[6] = '0';
+array[7] = '8';array[8] = '-';array[9] = '\0';
+array[10] = '\0'; array[11] = '\0'; break; } return;}
+
+for (int n = 0; n <=11; n++)array[n] = 0;	
+if ((sign = num) < 0) num = num * (-1);
+
+do {array[m] = num % radix;
+if(array[m] < 10)array[m] += '0'; else array[m] += '7';
+m++;} while ((num = num/radix) > 0);
+if (sign < 0) {array[m] = '-';m++;}}
+
+
+
+
+/*********************************************************************/
+void NumericString_to_PC(char* s){					
+int n=0;
+while (s[n] != '\0')n++;									//scroll to end of string counting the number of characters
+for(int m = n; m; m--)sendChar(*(s + m-1));}	
+
+
+
+
+/***************************************************************************************************************************************************/
+char decimal_digit (char data){
+if (((data > '9') || (data < '0')) )return 0;
+else return 1;}
 
 
 
@@ -91,59 +164,17 @@ return number;}
 
 
 
-/***************************************************************************************************************************************************/
-char wait_for_return_key(void){	
-char keypress;
-keypress = waitforkeypress();
-if((keypress == '\r') || (keypress == '\n')){
-if (isCharavailable(1)){receiveChar();}keypress = '\r';}
-return keypress;}
 
 
 
-/***************************************************************************************************************************************************/
-char decimal_digit (char data){
-if (((data > '9') || (data < '0')) )return 0;
-else return 1;}
 
 
 
-/*********************************************************************/
-void Num_to_PC(char radix, int int_num){
-char array[7];												//Int has 5 chars + sign + null terminator	
-SBtoAL(array, int_num, radix);								//calls the Binary to askii subroutine
-NumericString_to_PC(array);}								//Prints characters in reverse order
 
 
 
-/*********************************************************************/
-void SBtoAL(char array[], int num, char radix){			//Signed Binary to Askii
-int m=0;
-long sign;
-
-if (num == 0x8000){
-switch(radix){
-case 10: array[0] = '8'; array[1] = '6';
-array[2] = '7';array[3] = '2';array[4] = '3';
-array[5] = '-'; array[6] = '\0'; break;
-
-case 16: array[0] = '0';array[1] = '0'; array[2] = '0';	
-array[3] = '8';array[4] = '-';
-array[5] = '\0'; array[6] = '\0'; break; } return;}
-
-for (int n = 0; n <=6; n++)array[n] = 0;	
-if ((sign = num) < 0) num = num * (-1);
-
-do {array[m] = num % radix;
-if(array[m] < 10)array[m] += '0'; else array[m] += '7';
-m++;} while ((num = num/radix) > 0);
-if (sign < 0) {array[m] = '-';m++;}}
 
 
 
-/*********************************************************************/
-void NumericString_to_PC(char* s){					
-int n=0;
-while (s[n] != '\0')n++;									//scroll to end of string counting the number of characters
-for(int m = n; m; m--)sendChar(*(s + m-1));}	
+
 
