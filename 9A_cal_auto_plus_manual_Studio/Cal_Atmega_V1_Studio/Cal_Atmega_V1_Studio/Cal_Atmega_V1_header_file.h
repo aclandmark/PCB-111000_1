415cf76c8927accd39f645b0a62dd6c536cf9265
aclandmark <mark.osborne15@btinternet.com>
2020-08-15 17:35:04 +0100

    Update Cal_Atmega_V1_header_file.h

--- 9A_cal_auto_plus_manual_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_header_file.h
+++ 9A_cal_auto_plus_manual_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_header_file.h
@@ -1,8 +1,7 @@
 
 
 #include "Cal_resources/Basic_HW_setup.h"
-#include "Cal_resources/Basic_HW_plus_Timer.c"
-#include "Cal_resources/Basic_PC_comms.c"
+#include "Cal_resources/Basic_IO_and_timer_subs.c"
 #include "Cal_resources/Cal_subroutines.c"
 
 
