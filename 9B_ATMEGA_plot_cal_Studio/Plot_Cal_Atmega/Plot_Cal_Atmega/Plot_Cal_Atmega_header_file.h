6fe233a56adde31bca9da480a65b1606af2a64a8
aclandmark <mark.osborne15@btinternet.com>
2020-08-15 17:35:55 +0100

    Update Plot_Cal_Atmega_header_file.h

--- 9B_ATMEGA_plot_cal_Studio/Plot_Cal_Atmega/Plot_Cal_Atmega/Plot_Cal_Atmega_header_file.h
+++ 9B_ATMEGA_plot_cal_Studio/Plot_Cal_Atmega/Plot_Cal_Atmega/Plot_Cal_Atmega_header_file.h
@@ -1,11 +1,6 @@
 
 
 #include "../Cal_resources/Basic_HW_setup.h"
-#include "../Cal_resources/Basic_HW_plus_Timer.c"
-#include "../Cal_resources/Basic_PC_comms.c"
+#include "../Cal_resources/Basic_IO_and_timer_subs.c"
 #include "../Cal_resources/Cal_subroutines.c"
-
-
-
-//const char *Device = "168";
 
