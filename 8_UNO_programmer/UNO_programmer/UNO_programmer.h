4c9e40ebbd405d555e5e081241dec447530d0ac4
aclandmark <mark.osborne15@btinternet.com>
2020-08-27 14:48:31 +0100

    Update UNO_programmer.h

--- 8_UNO_programmer/UNO_programmer/UNO_programmer.h
+++ 8_UNO_programmer/UNO_programmer/UNO_programmer.h
@@ -5,8 +5,6 @@
 #include <avr/wdt.h>
 #include <avr/eeprom.h>
 
-//const char *Device = "168";
-
 #include "Resources_UNO_programmer\\UNO_Programmer.h"
 #include "Resources_UNO_programmer\Device_characteristics.c"
 #include "Resources_UNO_programmer\UNO_Programmer_HW_subs.c"
