00644c6769221102871a6e55133fa30cfab32348
aclandmark <mark.osborne15@btinternet.com>
2020-09-03 16:31:16 +0100

    Update Basic_HW_setup.h

--- 9A_cal_auto_plus_manual_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_Studio/Cal_resources/Basic_HW_setup.h
+++ 9A_cal_auto_plus_manual_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_Studio/Cal_resources/Basic_HW_setup.h
@@ -35,6 +35,7 @@ volatile long error_SUM;
 volatile char cal_mode; 							//Defines number of averages used when measuring osccal_error	
 volatile char T1_OVF;
 
+int sig_byte_2, sig_byte_3;
 
 
 /***************************************************************************/
@@ -122,10 +123,70 @@ cal_error = compute_error(0,cal_mode,0);
 
 
 
-
-
+/*****************************************************************************/
+const char *Device_type[8], *Device_family[2];
+int device_ptr, family_ptr;
 
 /*****************************************************************************/
+#define Set_device_signatures \
+Device_type[0] = "48/P";\
+Device_type[1] = "88/P";\
+Device_type[2] = "168/P";\
+Device_type[3] = "328/P";\
+Device_type[4] = "644";\
+Device_type[5] = "32A";\
+Device_type[6] = "44A";\
+\
+\
+Device_family[0] = "Atmega ";\
+Device_family[1] = "ATtiny ";
+
+/*****************************************************************************/
+#define set_device_type_and_memory_size \
+Set_device_signatures;\
+sig_byte_2 = eeprom_read_byte((uint8_t*)(EEP_MAX - 4));\
+sig_byte_3 = eeprom_read_byte((uint8_t*)(EEP_MAX - 5));\
+\
+switch(sig_byte_2){\
+	\
+	case 0x92: FlashSZ = 0x800;  EE_size = 0x100;\
+		switch (sig_byte_3)\
+			{case 0x05: \
+			case 0x0A: device_ptr = 0; family_ptr = 0; break;\
+			case 0x07: device_ptr = 5; family_ptr = 1; break;}\
+		break;\
+		\
+	case 0x93: FlashSZ = 0x1000; EE_size = 0x200;\
+		switch (sig_byte_3)\
+			{case 0x0A:\
+			case 0x0F: device_ptr = 1; family_ptr = 0; break;}\
+		break;\
+		\
+	case 0x94: FlashSZ = 0x2000; EE_size = 0x200;\
+		switch (sig_byte_3)\
+			{case 0x06: \
+			case 0x0B: device_ptr = 2; family_ptr = 0; break;}\
+		break;\
+	\
+	case 0x95: FlashSZ = 0x4000; EE_size = 0x400;\
+		switch (sig_byte_3)\
+			{case 0x14:\
+			case 0x0F: device_ptr = 3; family_ptr = 0; break;\
+			case 0x02: device_ptr = 6; family_ptr = 0; break;}\
+		break;\
+	\
+	case 0x96: FlashSZ = 0x8000; EE_size = 0x800;\
+		switch (sig_byte_3)\
+			{case 0x09: device_ptr = 4; family_ptr = 0; break;}\
+		break;}
+
+
+
+
+
+
+/*
+
 const char * Device_92 = "48/P";
 const char * Device_93 = "88/P";
 const char * Device_94 = "168/P";
@@ -136,9 +197,6 @@ const char * Device_type[6];
 int device_ptr;
 
 
-
-
-/*****************************************************************************/
 #define Set_device_signatures \
 Device_type[1] = Device_92;\
 Device_type[2] = Device_93;\
@@ -147,8 +205,6 @@ Device_type[4] = Device_95;\
 Device_type[5] = Device_96;
 
 
-
-/*****************************************************************************/
 #define set_device_type_and_memory_size \
 Set_device_signatures;\
 switch(eeprom_read_byte((uint8_t*)(EEP_MAX - 4))){\
@@ -157,7 +213,7 @@ switch(eeprom_read_byte((uint8_t*)(EEP_MAX - 4))){\
 	case 0x94: FlashSZ = 0x2000; EE_size = 0x200; device_ptr = 3; break;\
 	case 0x95: FlashSZ = 0x4000; EE_size = 0x400; device_ptr = 4; break;\
 	case 0x96: FlashSZ = 0x8000; EE_size = 0x800; device_ptr = 5; break;}
-
+*/
 
 
 
