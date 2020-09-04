8379555e6aa33f0ae2d27030166610cb579c44ea
aclandmark <mark.osborne15@btinternet.com>
2020-09-04 15:56:15 +0100

    Update Cal_Atmega32.c

--- 9C_cal_auto_plus_manual_32/Cal_Atmega32_V1/Cal_Atmega32_V1/Cal_Atmega32.c
+++ 9C_cal_auto_plus_manual_32/Cal_Atmega32_V1/Cal_Atmega32_V1/Cal_Atmega32.c
@@ -1,4 +1,29 @@
 
+#define UCSR0B UCSRB
+#define RXEN0 RXEN
+#define UDRE0 UDRE
+#define UBRR0H UBRRH
+#define UBRR0L UBRRL
+#define UDR0 UDR
+#define UCSR0A UCSRA
+#define U2X0 U2X
+#define TIMSK2 TIMSK
+#define TIMSK1 TIMSK
+#define TCR2BUB TCR2UB 
+#define TCCR2A TCCR2
+#define TCCR2B TCCR2
+#define TIFR0 TIFR
+#define TIFR1 TIFR
+#define  RXC0 RXC
+#define  TCCR0B TCCR0
+#define TXEN0 TXEN
+#define WDTCSR WDTCR
+#define WDCE WDTOE
+#define UCSR0C UCSRC
+#define UCSZ00 UCSZ0
+#define UCSZ01 UCSZ1
+#define TCR2AUB TCR2UB
+
 
 #include "Cal_Atmega_V1_header_file.h"
 
