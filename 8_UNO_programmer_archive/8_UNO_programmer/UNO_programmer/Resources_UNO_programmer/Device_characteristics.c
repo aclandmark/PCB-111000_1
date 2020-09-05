7c2c8bb3cbe66977bde1f3bb31b623cdef6c6546
aclandmark <mark.osborne15@btinternet.com>
2020-09-05 08:45:54 +0100

    Create Device_characteristics.c

---
+++ 8_UNO_programmer_archive/8_UNO_programmer/UNO_programmer/Resources_UNO_programmer/Device_characteristics.c
@@ -0,0 +1,106 @@
+
+void sendString(const char*);
+void sendChar(char);
+unsigned char Atmel_config ( unsigned int, unsigned int );
+void Detect_device_type(void);
+
+
+
+
+void Detect_device_type(void){
+
+Target_type = Atmel_config(signature_bit_2_h, signature_bit_2_l);
+switch(Target_type)\
+	{case 0x96: sendString("644"); break;
+	case 0x95: sendString("328"); break;
+	case 0x94: sendString("168"); break;
+	case 0x93: sendString("88"); break;
+	case 0x92: sendString("48"); break;
+	default: sendString("\r\nUnknown"); 
+	sendString("\r\n"); wdt_enable(WDTO_60MS);while(1);break;}}
+
+
+
+
+void set_up_target_parameters(void){
+
+
+if(Target_type == 0x96)									//Atmega 644
+{
+PageSZ = 0x80;
+PAmask = 0x7F80;
+FlashSZ = 0x8000;
+EE_size = 0x800;
+Target_type_M = 0x09;
+Fuse_Ex = 0xFF;
+Fuse_H = 0xD7;
+Fuse_L = 0xE2;
+Lock = 0xFF;}
+
+
+
+
+if(Target_type == 0x95)									//Atmega 328/P
+{
+PageSZ = 0x40;
+PAmask = 0x3FC0;
+FlashSZ = 0x4000;
+EE_size = 0x400;
+Target_type_M = 0x14;
+Target_type_P = 0x0F;
+Fuse_Ex = 0xFF;
+Fuse_H = 0xD7;
+Fuse_L = 0xE2;
+Lock = 0xFF;}
+
+
+
+if(Target_type == 0x94)									//Atmega 168/P
+{
+PageSZ = 0x40;			
+PAmask = 0x1FC0;		
+FlashSZ = 0x2000;		
+EE_size = 0x200;		
+Target_type_M = 0x06;		
+Target_type_P = 0x0B;	
+Fuse_Ex = 0xFF;
+Fuse_H = 0xD7;
+Fuse_L = 0xE2;
+Lock = 0xFF;}
+
+
+
+
+if(Target_type == 0x93)									//Atmega 88/P
+{
+PageSZ = 0x20;			
+PAmask = 0xFE0;		
+FlashSZ = 0x1000;		
+EE_size = 0x200;		
+Target_type_M = 0x0A;		
+Target_type_P = 0x0F;	
+Fuse_Ex = 0xFF;
+Fuse_H = 0xD7;
+Fuse_L = 0xE2;
+Lock = 0xFF;}
+
+
+
+
+if(Target_type == 0x92)									//Atmega 48/P
+{
+PageSZ = 0x20;			
+PAmask = 0x7E0;		
+FlashSZ = 0x800;		
+EE_size = 0x100;		
+Target_type_M = 0x05;		
+Target_type_P = 0x0A;	
+Fuse_Ex = 0xFF;
+Fuse_H = 0xD7;
+Fuse_L = 0xE2;
+Lock = 0xFF;}
+
+
+
+
+}
