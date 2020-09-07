ab1e037dc202ef57f413d063854bcf7780ef55ab
aclandmark <mark.osborne15@btinternet.com>
2020-09-07 07:59:49 +0100

    Update Basic_HW_setup.h

--- 9B_ATMEGA_plot_cal_Studio/Plot_Cal_Atmega/Cal_resources/Basic_HW_setup.h
+++ 9B_ATMEGA_plot_cal_Studio/Plot_Cal_Atmega/Cal_resources/Basic_HW_setup.h
@@ -29,6 +29,8 @@ volatile int EA_counter;
 volatile char cal_mode; 			//Defines number of averages used when measuring osccal_error	
 volatile char T1_OVF;
 
+int sig_byte_2, sig_byte_3;
+
 #define delay_2ms 4,195
 #define T0_delay_10ms 5,178
 #define T1_delay_1sec 5,0xE17D
@@ -43,16 +45,21 @@ volatile char T1_OVF;
 
 
 
-/*****************************************************************************/
-#define setup_HW_basic \
+/**********************************************************************************/
+#define setup_HW \
 setup_watchdog;\
-USART_init(0,25);\
-nop_delay(10);\
 ADMUX |= (1 << REFS0);\
-Timer_T0_sub(T0_delay_10ms);\
-initialise_IO;\
+Initialise_IO;\
 set_device_type_and_memory_size;\
-cal_device;
+cal_device;\
+\
+switch(sig_byte_2){\
+	case 0x95: if(sig_byte_3 != 2)USART_init(0,25);\
+	else USART_init_32(0,25); break;\
+default: USART_init(0,25); break;}
+
+
+
 
 /*****************************************************************************/
 #define SW_reset  {wdt_enable(WDTO_30MS);while(1);}
@@ -69,7 +76,7 @@ WDTCSR = 0;
 
 
 /*****************************************************************************/
-#define initialise_IO \
+#define Initialise_IO \
 MCUCR &= (~(1 << PUD));\
 DDRB = 0;\
 DDRC = 0;\
@@ -126,36 +133,63 @@ if((User_response == 'R') || (User_response == 'r'))break;} sendString("\r\n");
 
 
 /*****************************************************************************/
-const char * Device_92 = "48/P";
-const char * Device_93 = "88/P";
-const char * Device_94 = "168/P";
-const char * Device_95 = "328/P";
-const char * Device_96 = "644";
-
-const char * Device_type[6];
-int device_ptr;
-
-
+const char *Device_type[8], *Device_family[2];
+int device_ptr, family_ptr;
 
 
 /*****************************************************************************/
 #define Set_device_signatures \
-Device_type[1] = Device_92;\
-Device_type[2] = Device_93;\
-Device_type[3] = Device_94;\
-Device_type[4] = Device_95;\
-Device_type[5] = Device_96;
+Device_type[0] = "48/P";\
+Device_type[1] = "88/P";\
+Device_type[2] = "168/P";\
+Device_type[3] = "328/P";\
+Device_type[4] = "644";\
+Device_type[5] = "32A";\
+Device_type[6] = "44A";\
+\
+\
+Device_family[0] = "Atmega ";\
+Device_family[1] = "ATtiny ";
 
 
 
 /*****************************************************************************/
 #define set_device_type_and_memory_size \
 Set_device_signatures;\
-switch(eeprom_read_byte((uint8_t*)(EEP_MAX - 4))){\
-	case 0x92: FlashSZ = 0x800;  EE_size = 0x100; device_ptr = 1; break;\
-	case 0x93: FlashSZ = 0x1000; EE_size = 0x200; device_ptr = 2; break;\
-	case 0x94: FlashSZ = 0x2000; EE_size = 0x200; device_ptr = 3; break;\
-	case 0x95: FlashSZ = 0x4000; EE_size = 0x400; device_ptr = 4; break;\
-	case 0x96: FlashSZ = 0x8000; EE_size = 0x800; device_ptr = 5; break;}
-	
-	
+sig_byte_2 = eeprom_read_byte((uint8_t*)(EEP_MAX - 4));\
+sig_byte_3 = eeprom_read_byte((uint8_t*)(EEP_MAX - 5));\
+\
+switch(sig_byte_2){\
+	\
+	case 0x92: FlashSZ = 0x800;  EE_size = 0x100;\
+	switch (sig_byte_3)\
+	{case 0x05: \
+		case 0x0A: device_ptr = 0; family_ptr = 0; break;\
+	case 0x07: device_ptr = 6; family_ptr = 1; break;}\
+	break;\
+	\
+	case 0x93: FlashSZ = 0x1000; EE_size = 0x200;\
+	switch (sig_byte_3)\
+	{case 0x0A:\
+	case 0x0F: device_ptr = 1; family_ptr = 0; break;}\
+	break;\
+	\
+	case 0x94: FlashSZ = 0x2000; EE_size = 0x200;\
+	switch (sig_byte_3)\
+	{case 0x06: \
+	case 0x0B: device_ptr = 2; family_ptr = 0; break;}\
+	break;\
+	\
+	case 0x95: FlashSZ = 0x4000; EE_size = 0x400;\
+	switch (sig_byte_3)\
+	{case 0x14:\
+		case 0x0F: device_ptr = 3; family_ptr = 0; break;\
+	case 0x02: device_ptr = 5; family_ptr = 0; break;}\
+	break;\
+	\
+	case 0x96: FlashSZ = 0x8000; EE_size = 0x800;\
+	switch (sig_byte_3)\
+	{case 0x09: device_ptr = 4; family_ptr = 0; break;}\
+break;}
+
+
