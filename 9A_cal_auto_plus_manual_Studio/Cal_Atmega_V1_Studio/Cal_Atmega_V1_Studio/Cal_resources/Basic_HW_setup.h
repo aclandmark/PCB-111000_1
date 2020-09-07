df0e82c99f39c53a3c112e447aa9127140cbe0ba
aclandmark <mark.osborne15@btinternet.com>
2020-09-07 07:59:31 +0100

    Update Basic_HW_setup.h

--- 9A_cal_auto_plus_manual_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_Studio/Cal_resources/Basic_HW_setup.h
+++ 9A_cal_auto_plus_manual_Studio/Cal_Atmega_V1_Studio/Cal_Atmega_V1_Studio/Cal_resources/Basic_HW_setup.h
@@ -58,8 +58,14 @@ int sig_byte_2, sig_byte_3;
 #define setup_HW \
 setup_watchdog;\
 ADMUX |= (1 << REFS0);\
-USART_init(0,25);\
-Initialise_IO;
+Initialise_IO;\
+set_device_type_and_memory_size;\
+\
+switch(sig_byte_2){\
+	case 0x95: if(sig_byte_3 != 2)USART_init(0,25);\
+	else USART_init_32(0,25); break;\
+default: USART_init(0,25); break;}
+
 
 
 
@@ -153,7 +159,7 @@ switch(sig_byte_2){\
 		switch (sig_byte_3)\
 			{case 0x05: \
 			case 0x0A: device_ptr = 0; family_ptr = 0; break;\
-			case 0x07: device_ptr = 5; family_ptr = 1; break;}\
+			case 0x07: device_ptr = 6; family_ptr = 1; break;}\
 		break;\
 		\
 	case 0x93: FlashSZ = 0x1000; EE_size = 0x200;\
@@ -172,49 +178,11 @@ switch(sig_byte_2){\
 		switch (sig_byte_3)\
 			{case 0x14:\
 			case 0x0F: device_ptr = 3; family_ptr = 0; break;\
-			case 0x02: device_ptr = 6; family_ptr = 0; break;}\
+			case 0x02: device_ptr = 5; family_ptr = 0; break;}\
 		break;\
 	\
 	case 0x96: FlashSZ = 0x8000; EE_size = 0x800;\
 		switch (sig_byte_3)\
 			{case 0x09: device_ptr = 4; family_ptr = 0; break;}\
 		break;}
-
-
-
-
-
-
-/*
-
-const char * Device_92 = "48/P";
-const char * Device_93 = "88/P";
-const char * Device_94 = "168/P";
-const char * Device_95 = "328/P";
-const char * Device_96 = "644";
-
-const char * Device_type[6];
-int device_ptr;
-
-
-#define Set_device_signatures \
-Device_type[1] = Device_92;\
-Device_type[2] = Device_93;\
-Device_type[3] = Device_94;\
-Device_type[4] = Device_95;\
-Device_type[5] = Device_96;
-
-
-#define set_device_type_and_memory_size \
-Set_device_signatures;\
-switch(eeprom_read_byte((uint8_t*)(EEP_MAX - 4))){\
-	case 0x92: FlashSZ = 0x800;  EE_size = 0x100; device_ptr = 1; break;\
-	case 0x93: FlashSZ = 0x1000; EE_size = 0x200; device_ptr = 2; break;\
-	case 0x94: FlashSZ = 0x2000; EE_size = 0x200; device_ptr = 3; break;\
-	case 0x95: FlashSZ = 0x4000; EE_size = 0x400; device_ptr = 4; break;\
-	case 0x96: FlashSZ = 0x8000; EE_size = 0x800; device_ptr = 5; break;}
-*/
-
-
-
 
