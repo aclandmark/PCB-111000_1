4851a355d74dc41e4e7c04cce42b5e31db735b37
aclandmark <mark.osborne15@btinternet.com>
2020-10-10 15:16:35 +0100

    Update Device_characteristics.c

--- 8_UNO_AVR_Programmer_V2/Device_characteristics.c
+++ 8_UNO_AVR_Programmer_V2/Device_characteristics.c
@@ -7,11 +7,11 @@
 #define SW_reset {wdt_enable(WDTO_30MS);while(1);}
 
 int sig_byte_2, sig_byte_3;
-signed int PageSZ;												//Size of a page of flash in 16 bit words
-signed int PAmask;												//Used to obtain the flash page address from the hex address
-unsigned int FlashSZ;											//Amount of flash memory in 16 bit words supplied on target device
-int EE_size;													//EEPROM size
-unsigned char Fuse_Ex, Fuse_H, Fuse_L, Lock;
+signed int PageSZ;												        //Size of a page of flash in 16 bit words
+signed int PAmask;												        //Used to obtain the flash page address from the hex address
+unsigned int FlashSZ;											        //Amount of flash memory in 16 bit words supplied on target device
+int EE_size;													            //EEPROM size
+unsigned char Fuse_Ex, Fuse_H, Fuse_L, Lock;      //All fuse (configuration byte)setings can be modified by the user
 
 char Twd_EEPROM;
 
@@ -35,21 +35,21 @@ const char *Atmega = "\r\nAtmega ";
 const char *ATtiny = "\r\nATtiny ";
 const char *Unrecognised_device = "\r\nUnrecognised_device:  ";
 
-Twd_EEPROM = 5;																//default delay in mS
+Twd_EEPROM = 5;																            //default delay in mS
 
 sig_byte_2 = Atmel_config(signature_bit_2_h, signature_bit_2_l);
 sig_byte_3 = Atmel_config(signature_bit_3_h, signature_bit_3_l);
 
 
 switch(sig_byte_2)
-	{case 0x96: 															//64KB devices
+	{case 0x96: 															            //64KB devices
 	switch (sig_byte_3)
 	{case 0x09: sendString(Atmega); sendString(" 644");break;	
 	default: sendString(Unrecognised_device);sendString("64KB");
 	newline();SW_reset;break;} 
 	
 	PageSZ = 0x80;
-	PAmask = 0x7F80;														//=FlashSZ - PageSZ
+	PAmask = 0x7F80;														          //=FlashSZ - PageSZ
 	FlashSZ = 0x8000;
 	EE_size = 0x800;
 	Fuse_Ex = 0xFF;
@@ -68,7 +68,7 @@ switch(sig_byte_2)
 	newline();SW_reset;break;}
 	 
 	PageSZ = 0x40;
-	PAmask = 0x3FC0;														//=FlashSZ - PageSZ
+	PAmask = 0x3FC0;														            //=FlashSZ - PageSZ
 	FlashSZ = 0x4000;
 	EE_size = 0x400;
 	Fuse_Ex = 0xFF;
@@ -82,7 +82,7 @@ switch(sig_byte_2)
 	break;
 	
 	
-	case 0x94:  															//16KB devices
+	case 0x94:  															            //16KB devices
 	switch (sig_byte_3)
 	{case 0x06: sendString(Atmega);sendString("168");break;
 	case 0x0B: sendString(Atmega);sendString("168P");break;
@@ -90,7 +90,7 @@ switch(sig_byte_2)
 	newline();SW_reset;break;}
 	
 	PageSZ = 0x40;			
-	PAmask = 0x1FC0;														//=FlashSZ - PageSZ		
+	PAmask = 0x1FC0;														            //=FlashSZ - PageSZ		
 	FlashSZ = 0x2000;		
 	EE_size = 0x200;		
 	Fuse_Ex = 0xFF;
@@ -100,7 +100,7 @@ switch(sig_byte_2)
 	break;
 	
 	
-	case 0x93:  																//8KB devices
+	case 0x93:  																            //8KB devices
 	switch (sig_byte_3)
 	{case 0x0A: sendString(Atmega);sendString("88");break;
 	case 0x0F: sendString(Atmega);sendString("88P");break;
@@ -110,7 +110,7 @@ switch(sig_byte_2)
 	newline();SW_reset;break;}
 	 
 	PageSZ = 0x20;			
-	PAmask = 0xFE0;																//=FlashSZ - PageSZ	
+	PAmask = 0xFE0;																            //=FlashSZ - PageSZ	
 	FlashSZ = 0x1000;		
 	EE_size = 0x200;		
 	Fuse_Ex = 0xFF;
@@ -121,7 +121,7 @@ switch(sig_byte_2)
 	
 	
 	
-	case 0x92: 																	//4KB devices 
+	case 0x92: 																	            //4KB devices 
 	switch (sig_byte_3)
 	{case 0x05: sendString(Atmega);sendString("48");break;
 	case 0x0A: sendString(Atmega);sendString("48P");break;
@@ -131,7 +131,7 @@ switch(sig_byte_2)
 	newline();SW_reset;break;}
 	
 	PageSZ = 0x20;      
-  PAmask = 0x7E0;                               //=FlashSZ - PageSZ   
+  PAmask = 0x7E0;                                           //=FlashSZ - PageSZ   
   FlashSZ = 0x800;    
   EE_size = 0x100;    
   Fuse_Ex = 0xFF;
@@ -149,7 +149,7 @@ case 0x91:
   newline();SW_reset;break;}
   
   PageSZ =   0x10;    
-  PAmask =   0x3F0;                          //=FlashSZ - PageSZ   
+  PAmask =   0x3F0;                                     //=FlashSZ - PageSZ   
   FlashSZ =  0x400;   
   EE_size =  0x80; 
   Fuse_Ex = 0xFF;
